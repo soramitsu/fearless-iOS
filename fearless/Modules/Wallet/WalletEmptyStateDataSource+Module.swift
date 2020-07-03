4d9d896f35ab89aafa27da01e95ea2f365ad0c96
Russel <emkil.russel@gmail.com>
2020-07-03 14:08:59 +0300

    fix icon

--- fearless/Modules/Wallet/WalletEmptyStateDataSource+Module.swift
+++ fearless/Modules/Wallet/WalletEmptyStateDataSource+Module.swift
@@ -7,7 +7,9 @@ extension WalletEmptyStateDataSource {
             R.string.localizable.walletEmptyDescription(preferredLanguages: locale.rLanguages)
         }
 
-        let image = R.image.transactionsEmptyState()
+        let image = R.image.iconAbout()?
+            .tinted(with: UIColor.iconTintColor)?
+            .withRenderingMode(.alwaysOriginal)
         let dataSource = WalletEmptyStateDataSource(titleResource: title, image: image)
         dataSource.localizationManager = LocalizationManager.shared
 
