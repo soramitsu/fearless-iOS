6305225df6e705413105012a9016b189d2948c47
Russel <emkil.russel@gmail.com>
2020-09-11 14:54:02 +0300

    apply design from mockups

--- fearless/Common/Model/Language.swift
+++ fearless/Common/Model/Language.swift
@@ -14,4 +14,14 @@ extension Language {
             return nil
         }
     }
+
+    func region(in locale: Locale) -> String? {
+        let components = Locale.components(fromIdentifier: code)
+
+        if let regionCode = components[NSLocale.Key.countryCode.rawValue] {
+            return locale.localizedString(forRegionCode: regionCode)
+        } else {
+            return nil
+        }
+    }
 }
