b367581cf82d2057b4be1d2aa21109f4fe840f36
Russel <emkil.russel@gmail.com>
2020-10-05 12:26:28 +0300

    reload wallet view when connection changes

--- fearless/Modules/MainTabBar/MainTabBarInteractor.swift
+++ fearless/Modules/MainTabBar/MainTabBarInteractor.swift
@@ -1,12 +1,25 @@
 import Foundation
+import SoraKeystore
 
 final class MainTabBarInteractor {
 	weak var presenter: MainTabBarInteractorOutputProtocol?
 
     let eventCenter: EventCenterProtocol
+    let settings: SettingsManagerProtocol
 
-    init(eventCenter: EventCenterProtocol) {
+    private var currentAccount: AccountItem?
+    private var currentConnection: ConnectionItem?
+
+    init(eventCenter: EventCenterProtocol, settings: SettingsManagerProtocol) {
         self.eventCenter = eventCenter
+        self.settings = settings
+
+        updateSelectedItems()
+    }
+
+    private func updateSelectedItems() {
+        self.currentAccount = settings.selectedAccount
+        self.currentConnection = settings.selectedConnection
     }
 }
 
@@ -18,6 +31,16 @@ extension MainTabBarInteractor: MainTabBarInteractorInputProtocol {
 
 extension MainTabBarInteractor: EventVisitorProtocol {
     func processSelectedAccountChanged(event: SelectedAccountChanged) {
-        presenter?.didReloadSelectedAccount()
+        if currentAccount != settings.selectedAccount {
+            updateSelectedItems()
+            presenter?.didReloadSelectedAccount()
+        }
+    }
+
+    func processSelectedConnectionChanged(event: SelectedConnectionChanged) {
+        if currentConnection != settings.selectedConnection {
+            updateSelectedItems()
+            presenter?.didReloadSelectedNetwork()
+        }
     }
 }
