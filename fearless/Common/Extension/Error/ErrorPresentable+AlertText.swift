8586e4b93db0f8bb900977ef8f5d1dde2b12f0d7
Aleksandr Malina <a.malina@fbs.com>
2021-02-23 12:51:33 +0300

    Review ErrorPresentable

--- fearless/Common/Extension/Error/ErrorPresentable+AlertText.swift
+++ fearless/Common/Extension/Error/ErrorPresentable+AlertText.swift
@@ -12,25 +12,27 @@ protocol ErrorContentConvertible {
 
 extension ErrorPresentable where Self: AlertPresentable {
     func present(error: Error, from view: ControllerBackedProtocol?, locale: Locale?) -> Bool {
-        var optionalContent: ErrorContent?
+        let optionalContent: ErrorContent? = {
+            if let contentConvertibleError = error as? ErrorContentConvertible {
+                return contentConvertibleError.toErrorContent(for: locale)
+            }
 
-        if let contentConvertibleError = error as? ErrorContentConvertible {
-            optionalContent = contentConvertibleError.toErrorContent(for: locale)
-        }
+            if error as? BaseOperationError != nil {
+                let title = R.string.localizable.operationErrorTitle(preferredLanguages: locale?.rLanguages)
+                let message = R.string.localizable.operationErrorMessage(preferredLanguages: locale?.rLanguages)
 
-        if error as? BaseOperationError != nil {
-            let title = R.string.localizable.operationErrorTitle(preferredLanguages: locale?.rLanguages)
-            let message = R.string.localizable.operationErrorMessage(preferredLanguages: locale?.rLanguages)
+                return ErrorContent(title: title, message: message)
+            }
 
-            optionalContent = ErrorContent(title: title, message: message)
-        }
+            if (error as NSError).domain == NSURLErrorDomain {
+                let title = R.string.localizable.connectionErrorTitle(preferredLanguages: locale?.rLanguages)
+                let message = R.string.localizable.connectionErrorMessage(preferredLanguages: locale?.rLanguages)
 
-        if (error as NSError).domain == NSURLErrorDomain {
-            let title = R.string.localizable.connectionErrorTitle(preferredLanguages: locale?.rLanguages)
-            let message = R.string.localizable.connectionErrorMessage(preferredLanguages: locale?.rLanguages)
+                return ErrorContent(title: title, message: message)
+            }
 
-            optionalContent = ErrorContent(title: title, message: message)
-        }
+            return nil
+        }()
 
         guard let content = optionalContent else {
             return false
