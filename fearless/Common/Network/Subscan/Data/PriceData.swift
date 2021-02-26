290b047df6e11e100f13ee8f52db744f52b97b54
Aleksandr Malina <a.malina@fbs.com>
2021-02-26 11:55:04 +0300

    Revert PriceData changes

--- fearless/Common/Network/Subscan/Data/PriceData.swift
+++ fearless/Common/Network/Subscan/Data/PriceData.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-struct PriceData: Decodable {
+struct PriceData: Codable, Equatable {
     let price: String
     let time: Int64
     let height: Int64
     let records: [PriceRecord]
 }
 
-struct PriceRecord: Decodable {
+struct PriceRecord: Codable, Equatable {
     let price: String
     let time: Int64
     let height: Int64
