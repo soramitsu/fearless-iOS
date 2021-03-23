e16f69993beb66a0536019021969ee3f6d090812
Aleksandr Malina <ef1rspb@gmail.com>
2021-03-23 16:33:47 +0300

    Fix review notes

--- fearless/Common/Extension/UIKit/TriangularedButton+Style.swift
+++ fearless/Common/Extension/UIKit/TriangularedButton+Style.swift
@@ -4,8 +4,8 @@ import SoraUI
 extension TriangularedButton {
     func applyDefaultStyle() {
         triangularedView?.shadowOpacity = 0.0
-        triangularedView?.fillColor = R.color.colorPink()!
-        triangularedView?.highlightedFillColor = R.color.colorPink()!
+        triangularedView?.fillColor = R.color.colorAccent()!
+        triangularedView?.highlightedFillColor = R.color.colorAccent()!
         triangularedView?.strokeColor = .clear
         triangularedView?.highlightedStrokeColor = .clear
 
