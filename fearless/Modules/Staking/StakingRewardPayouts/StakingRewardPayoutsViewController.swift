dce72762c1d261981cce92c86a0c34933cbf4fde
Aleksandr Malina <ef1rspb@gmail.com>
2021-03-25 10:48:43 +0300

    Define VC init

--- fearless/Modules/Staking/StakingRewardPayouts/StakingRewardPayoutsViewController.swift
+++ fearless/Modules/Staking/StakingRewardPayouts/StakingRewardPayoutsViewController.swift
@@ -2,8 +2,21 @@ import UIKit
 import SoraFoundation
 
 final class StakingRewardPayoutsViewController: UIViewController {
-    var presenter: StakingRewardPayoutsPresenterProtocol!
 
+    // MARK: Properties -
+    let presenter: StakingRewardPayoutsPresenterProtocol
+
+    // MARK: Init -
+    init(presenter: StakingRewardPayoutsPresenterProtocol) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Lifecycle -
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- fearless/Modules/Staking/StakingRewardPayouts/StakingRewardPayoutsViewFactory.swift
+++ fearless/Modules/Staking/StakingRewardPayouts/StakingRewardPayoutsViewFactory.swift
@@ -2,12 +2,11 @@ import Foundation
 
 final class StakingRewardPayoutsViewFactory: StakingRewardPayoutsViewFactoryProtocol {
     static func createView() -> StakingRewardPayoutsViewProtocol? {
-        let view = StakingRewardPayoutsViewController()
         let presenter = StakingRewardPayoutsPresenter()
+        let view = StakingRewardPayoutsViewController(presenter: presenter)
         let interactor = StakingRewardPayoutsInteractor()
         let wireframe = StakingRewardPayoutsWireframe()
 
-        view.presenter = presenter
         presenter.view = view
         presenter.interactor = interactor
         presenter.wireframe = wireframe
