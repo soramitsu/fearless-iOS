c9d6236df4ff70d7c3e5af6462100aab2bf1ed85
Russel <emkil.russel@gmail.com>
2021-03-25 14:15:56 +0300

    refactor status display

--- fearless/Modules/Staking/StakingMain/StateMachine/States/NominatorState+Status.swift
+++ fearless/Modules/Staking/StakingMain/StateMachine/States/NominatorState+Status.swift
@@ -1,5 +1,6 @@
 import Foundation
 import IrohaCrypto
+import BigInt
 
 extension NominatorState {
     var status: NominationViewStatus {
@@ -30,5 +31,93 @@ extension NominatorState {
         } catch {
             return .undefined
         }
+    }
+
+    func createStatusPresentableViewModel(for minimumStake: BigUInt?,
+                                          locale: Locale?) -> AlertPresentableViewModel? {
+        switch status {
+        case .active:
+            return createActiveStatus(for: minimumStake, locale: locale)
+        case .inactive:
+            return createInactiveStatus(for: minimumStake, locale: locale)
+        case .waiting:
+            return createWaitingStatus(for: minimumStake, locale: locale)
+        case .election:
+            return createElectionStatus(for: minimumStake, locale: locale)
+        case .undefined:
+            return createUndefinedStatus(for: minimumStake, locale: locale)
+        }
+    }
+
+    private func createActiveStatus(for minimumStake: BigUInt?,
+                                    locale: Locale?) -> AlertPresentableViewModel? {
+        let closeAction = R.string.localizable.commonClose(preferredLanguages: locale?.rLanguages)
+        let title = R.string.localizable
+            .stakingNominatorStatusAlertActiveTitle(preferredLanguages: locale?.rLanguages)
+        let message = R.string.localizable
+            .stakingNominatorStatusAlertActiveMessage(preferredLanguages: locale?.rLanguages)
+
+        return AlertPresentableViewModel(title: title,
+                                         message: message,
+                                         actions: [],
+                                         closeAction: closeAction)
+    }
+
+    private func createInactiveStatus(for minimumStake: BigUInt?,
+                                      locale: Locale?) -> AlertPresentableViewModel? {
+        guard let minimumStake = minimumStake else {
+            return nil
+        }
+
+        let closeAction = R.string.localizable.commonClose(preferredLanguages: locale?.rLanguages)
+        let title = R.string.localizable
+            .stakingNominatorStatusAlertInactiveTitle(preferredLanguages: locale?.rLanguages)
+        let message: String
+
+        if ledgerInfo.active < minimumStake {
+            message = R.string.localizable
+                .stakingNominatorStatusAlertLowStake(preferredLanguages: locale?.rLanguages)
+        } else {
+            message = R.string.localizable
+                .stakingNominatorStatusAlertNoValidators(preferredLanguages: locale?.rLanguages)
+        }
+
+        return AlertPresentableViewModel(title: title,
+                                         message: message,
+                                         actions: [],
+                                         closeAction: closeAction)
+    }
+
+    private func createWaitingStatus(for minimumStake: BigUInt?,
+                                     locale: Locale?) -> AlertPresentableViewModel? {
+        let closeAction = R.string.localizable.commonClose(preferredLanguages: locale?.rLanguages)
+        let title = R.string.localizable
+            .stakingNominatorStatusWaiting(preferredLanguages: locale?.rLanguages)
+        let message = R.string.localizable
+            .stakingNominatorStatusAlertWaitingMessage(preferredLanguages: locale?.rLanguages)
+
+        return AlertPresentableViewModel(title: title,
+                                         message: message,
+                                         actions: [],
+                                         closeAction: closeAction)
+    }
+
+    private func createElectionStatus(for minimumStake: BigUInt?,
+                                      locale: Locale?) -> AlertPresentableViewModel? {
+        let closeAction = R.string.localizable.commonClose(preferredLanguages: locale?.rLanguages)
+        let title = R.string.localizable
+            .stakingNominatorStatusElection(preferredLanguages: locale?.rLanguages)
+        let message = R.string.localizable
+            .stakingNominatorStatusAlertElectionMessage(preferredLanguages: locale?.rLanguages)
+
+        return AlertPresentableViewModel(title: title,
+                                         message: message,
+                                         actions: [],
+                                         closeAction: closeAction)
+    }
+
+    private func createUndefinedStatus(for minimumStake: BigUInt?,
+                                       locale: Locale?) -> AlertPresentableViewModel? {
+        return nil
     }
 }
