6eddb9ebef0b3520f93ca73d32950c67ea6b82de
Russel <emkil.russel@gmail.com>
2021-03-26 23:32:35 +0300

    fix tests

--- fearlessTests/Modules/Wallet/Commands/WalletSelectAccountCommandTests.swift
+++ fearlessTests/Modules/Wallet/Commands/WalletSelectAccountCommandTests.swift
@@ -9,63 +9,16 @@ class WalletSelectAccountCommandTests: XCTestCase {
     func testSelectAccount() throws {
         // given
 
-        let facade = UserDataStorageTestFacade()
-
-        let settings = InMemorySettingsManager()
-        let keychain = InMemoryKeychain()
-
-        let seed1 = Data(repeating: 0, count: 32)
-        let seed2 = Data(repeating: 1, count: 32)
-
-        try AccountCreationHelper.createAccountFromSeed(seed1.toHex(),
-                                                        cryptoType: .sr25519,
-                                                        keychain: keychain,
-                                                        settings: settings)
-
-        let account1 = settings.selectedAccount!
-
-        try AccountCreationHelper.createAccountFromSeed(seed2.toHex(),
-                                                        cryptoType: .sr25519,
-                                                        keychain: keychain,
-                                                        settings: settings)
-
-        let account2 = settings.selectedAccount!
-
-        let accountsRepository: CoreDataRepository<AccountItem, CDAccountItem> = facade.createRepository()
-        let operation = accountsRepository.saveOperation({ [account1, account2]}, { [] })
-
-        OperationQueue().addOperations([operation], waitUntilFinished: true)
-
-        let repositoryFactory = MockAccountRepositoryFactoryProtocol()
-
-        stub(repositoryFactory) { stub in
-            when(stub).createAccountRepository(for: any())
-                .thenReturn(AnyDataProviderRepository(accountsRepository))
-            when(stub).operationManager.get.thenReturn(OperationManager())
-        }
-
         let commandFactory = WalletCommandFactoryProtocolMock()
-
-        let eventCenter = MockEventCenterProtocol()
 
         // when
 
-        let command = WalletSelectAccountCommand(repositoryFactory: repositoryFactory,
-                                                 commandFactory: commandFactory,
-                                                 settings: settings,
-                                                 eventCenter: eventCenter,
-                                                 localizationManager: LocalizationManager.shared)
+        let command = WalletSelectAccountCommand(commandFactory: commandFactory)
 
         let completionExpectation = XCTestExpectation()
 
-        stub(eventCenter) { stub in
-            when(stub).notify(with: any()).then { _ in
-                completionExpectation.fulfill()
-            }
-        }
-
         commandFactory.presentationClosure = { _ in
-            command.modalPickerDidSelectModelAtIndex(0, context: [account1, account2] as NSArray)
+            completionExpectation.fulfill()
 
             return WalletPresentationCommandProtocolMock()
         }
@@ -75,7 +28,5 @@ class WalletSelectAccountCommandTests: XCTestCase {
         // then
 
         wait(for: [completionExpectation], timeout: Constants.defaultExpectationDuration)
-
-        XCTAssertEqual(settings.selectedAccount, account1)
     }
 }
