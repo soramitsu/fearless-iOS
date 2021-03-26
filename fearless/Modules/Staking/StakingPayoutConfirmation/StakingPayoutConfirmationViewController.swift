b31a4b6131138b50d26f0bd95fc2919d8025e666
Aleksandr Malina <ef1rspb@gmail.com>
2021-03-26 14:42:15 +0300

    Add blank view layout

--- fearless/Modules/Staking/StakingPayoutConfirmation/StakingPayoutConfirmationViewController.swift
+++ fearless/Modules/Staking/StakingPayoutConfirmation/StakingPayoutConfirmationViewController.swift
@@ -1,11 +1,19 @@
 import UIKit
+import SoraFoundation
 
-final class StakingPayoutConfirmationViewController: UIViewController {
+final class StakingPayoutConfirmationViewController: UIViewController, ViewHolder {
+
+    typealias RootViewType = StakingPayoutConfirmationViewLayout
 
     let presenter: StakingPayoutConfirmationPresenterProtocol
+    let localizationManager: LocalizationManagerProtocol?
 
-    init(presenter: StakingPayoutConfirmationPresenterProtocol) {
+    init(
+        presenter: StakingPayoutConfirmationPresenterProtocol,
+        localizationManager: LocalizationManagerProtocol?
+    ) {
         self.presenter = presenter
+        self.localizationManager = localizationManager
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -13,11 +21,116 @@ final class StakingPayoutConfirmationViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func loadView() {
+        view = StakingPayoutConfirmationViewLayout()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        applyLocalization()
+        setupTable()
         presenter.setup()
+    }
+
+    private func setupTable() {
+        rootView.tableView.registerClassesForCell([
+            StakingRewardDetailsLabelTableCell.self,
+            StakingRewardDetailsRewardTableCell.self,
+            AccountInfoTableViewCell.self
+        ])
+        rootView.tableView.delegate = self
+        rootView.tableView.dataSource = self
     }
 }
 
 extension StakingPayoutConfirmationViewController: StakingPayoutConfirmationViewProtocol {}
+
+extension StakingPayoutConfirmationViewController: Localizable {
+
+    private func setupLocalization() {
+        setupTitleLocalization()
+        setupButtonLocalization()
+    }
+
+    func applyLocalization() {
+        if isViewLoaded {
+            setupLocalization()
+            rootView.tableView.reloadData()
+            view.setNeedsLayout()
+        }
+    }
+
+    private func setupTitleLocalization() {
+        let locale = localizationManager?.selectedLocale ?? Locale.current
+        title = R.string.localizable.stakingRewardDetailsTitle(preferredLanguages: locale.rLanguages)
+    }
+
+    private func setupButtonLocalization() {
+        let title = R.string.localizable.stakingRewardDetailsPayout()
+        rootView.confirmButton.imageWithTitleView?.title = title
+    }
+}
+
+extension StakingPayoutConfirmationViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        // TODO FLW-677
+    }
+}
+
+extension StakingPayoutConfirmationViewController: UITableViewDataSource {
+
+    // TODO delete stub data
+    var stubCellData: [RewardDetailsRow] {
+        return [
+            .validatorInfo(.init(
+                            name: "Validator",
+                            address: "✨👍✨ Day7 ✨👍✨",
+                            icon: R.image.iconAccount())),
+            .validatorInfo(.init(
+                            name: "Payout account",
+                            address: "🐟 ANDREY",
+                            icon: R.image.iconAccount())),
+            .era(.init(
+                    titleText: R.string.localizable.stakingRewardDetailsEra(),
+                    valueText: "#1690")),
+            .reward(.init(ksmAmountText: "0.00005 KSM", usdAmountText: "$0,01")),
+
+        ]
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        stubCellData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO handle current locale
+        switch stubCellData[indexPath.row] {
+        case .date(let dateViewModel):
+            let cell = tableView.dequeueReusableCellWithType(
+                StakingRewardDetailsLabelTableCell.self)!
+            cell.bind(model: dateViewModel)
+            return cell
+        case .era(let eraViewModel):
+            let cell = tableView.dequeueReusableCellWithType(
+                StakingRewardDetailsLabelTableCell.self)!
+            cell.bind(model: eraViewModel)
+            return cell
+        case .reward(let rewardViewModel):
+            let cell = tableView.dequeueReusableCellWithType(
+                StakingRewardDetailsRewardTableCell.self)!
+            cell.bind(model: rewardViewModel)
+            return cell
+        case .validatorInfo(let model):
+            let cell = tableView.dequeueReusableCellWithType(
+                AccountInfoTableViewCell.self)!
+            cell.bind(model: model)
+            return cell
+        default:
+            fatalError()
+        }
+    }
+}
--- fearless/Modules/Staking/StakingPayoutConfirmation/StakingPayoutConfirmationViewFactory.swift
+++ fearless/Modules/Staking/StakingPayoutConfirmation/StakingPayoutConfirmationViewFactory.swift
@@ -1,15 +1,17 @@
 import Foundation
+import SoraFoundation
 
 final class StakingPayoutConfirmationViewFactory: StakingPayoutConfirmationViewFactoryProtocol {
 
     static func createView() -> StakingPayoutConfirmationViewProtocol? {
         let presenter = StakingPayoutConfirmationPresenter()
-        let view = StakingPayoutConfirmationViewController(presenter: presenter)
+        let view = StakingPayoutConfirmationViewController(
+            presenter: presenter,
+            localizationManager: LocalizationManager.shared)
 
         let interactor = StakingPayoutConfirmationInteractor()
         let wireframe = StakingPayoutConfirmationWireframe()
 
-        presenter.view = view
         presenter.interactor = interactor
         presenter.wireframe = wireframe
         interactor.presenter = presenter
