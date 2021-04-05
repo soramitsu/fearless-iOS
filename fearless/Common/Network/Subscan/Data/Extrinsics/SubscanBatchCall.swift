d85ed63409a2bcee1db8c2e199c919094fe4fc25
Aleksandr Malina <ef1rspb@gmail.com>
2021-04-05 10:04:45 +0300

    Group Extrinsics

--- fearless/Common/Network/Subscan/Data/Extrinsics/SubscanBatchCall.swift
+++ fearless/Common/Network/Subscan/Data/Extrinsics/SubscanBatchCall.swift
@@ -1,38 +1,6 @@
-import Foundation
 import FearlessUtils
+import Foundation
 import IrohaCrypto
-
-struct SubscanExtrinsicsData: Decodable {
-    let count: Int
-    let extrinsics: [SubscanExtrinsicsItemData]?
-}
-
-struct SubscanExtrinsicsItemData: Decodable {
-    let params: JSON?
-}
-
-struct SubscanBondCall {
-    let controller: String
-
-    private struct InnerRepresentation: Decodable {
-        let name: String
-        let type: String
-        let value: String
-    }
-
-    init?(callArgs: JSON, chain: Chain) {
-        guard let data = callArgs.stringValue?.data(using: .utf8) else { return nil }
-        guard let array = try? JSONDecoder().decode([InnerRepresentation]?.self, from: data) else { return nil }
-        guard let controller = array.first(
-            where: { $0.name == "controller" && $0.type == "Address" }) else { return nil }
-        guard let controllerAddressData = try? Data(hexString: controller.value) else { return nil }
-        guard let controllerAddress = try? SS58AddressFactory().addressFromAccountId(
-            data: controllerAddressData,
-            type: chain.addressType
-        ) else { return nil }
-        self.controller = controllerAddress
-    }
-}
 
 struct SubscanBatchCall {
     let controllers: [String]
---
+++ fearless/Common/Network/Subscan/Data/Extrinsics/SubscanBondCall.swift
@@ -0,0 +1,26 @@
+import FearlessUtils
+import Foundation
+import IrohaCrypto
+
+struct SubscanBondCall {
+    let controller: String
+
+    private struct InnerRepresentation: Decodable {
+        let name: String
+        let type: String
+        let value: String
+    }
+
+    init?(callArgs: JSON, chain: Chain) {
+        guard let data = callArgs.stringValue?.data(using: .utf8) else { return nil }
+        guard let array = try? JSONDecoder().decode([InnerRepresentation]?.self, from: data) else { return nil }
+        guard let controller = array.first(
+            where: { $0.name == "controller" && $0.type == "Address" }) else { return nil }
+        guard let controllerAddressData = try? Data(hexString: controller.value) else { return nil }
+        guard let controllerAddress = try? SS58AddressFactory().addressFromAccountId(
+            data: controllerAddressData,
+            type: chain.addressType
+        ) else { return nil }
+        self.controller = controllerAddress
+    }
+}
