7fecc3fd6a537854d484fb9703e1e29e86a4dbd3
Aleksandr Malina <ef1rspb@gmail.com>
2021-05-12 15:53:13 +0300

    FLW-813 Update available connections on Networks screen

--- fearless/Common/Extension/Model/ConnectionItem+Default.swift
+++ fearless/Common/Extension/Model/ConnectionItem+Default.swift
@@ -18,8 +18,13 @@ extension ConnectionItem {
                 type: SNAddressType.kusamaMain
             ),
             ConnectionItem(
-                title: "Kusama, Web3 Foundation node",
-                url: URL(string: "wss://cc3-5.kusama.network")!,
+                title: "Kusama, OnFinality node",
+                url: URL(string: "wss://kusama.api.onfinality.io/public-ws")!,
+                type: SNAddressType.kusamaMain
+            ),
+            ConnectionItem(
+                title: "Kusama, Patract node",
+                url: URL(string: "wss://kusama.elara.patract.io")!,
                 type: SNAddressType.kusamaMain
             ),
             ConnectionItem(
@@ -28,8 +33,13 @@ extension ConnectionItem {
                 type: SNAddressType.polkadotMain
             ),
             ConnectionItem(
-                title: "Polkadot, Web3 Foundation node",
-                url: URL(string: "wss://cc1-1.polkadot.network")!,
+                title: "Polkadot, OnFinality node",
+                url: URL(string: "wss://polkadot.api.onfinality.io/public-ws")!,
+                type: SNAddressType.polkadotMain
+            ),
+            ConnectionItem(
+                title: "Polkadot, Patract node",
+                url: URL(string: "wss://polkadot.elara.patract.io")!,
                 type: SNAddressType.polkadotMain
             ),
             ConnectionItem(
