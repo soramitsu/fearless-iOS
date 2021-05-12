03048d3f3857ff3d88213634733814c7e3f01b20
Aleksandr Malina <ef1rspb@gmail.com>
2021-05-12 09:07:25 +0300

    Update layout

--- fearless/Modules/Staking/ControllerAccountConfirmation/View/ControllerAccountConfirmationLayout.swift
+++ fearless/Modules/Staking/ControllerAccountConfirmation/View/ControllerAccountConfirmationLayout.swift
@@ -1,25 +1,18 @@
 import UIKit
 
 final class ControllerAccountConfirmationLayout: UIView {
-    let stackView: UIStackView = {
-        let view = UIStackView()
-        view.isLayoutMarginsRelativeArrangement = true
-        view.layoutMargins = UIEdgeInsets(top: 16.0, left: 0.0, bottom: 0.0, right: 0.0)
-        view.axis = .vertical
-        view.alignment = .center
-        view.distribution = .fill
+    let containerView: ScrollableContainerView = {
+        let view = ScrollableContainerView()
+        view.stackView.isLayoutMarginsRelativeArrangement = true
+        view.stackView.layoutMargins = UIEdgeInsets(top: 16.0, left: 0.0, bottom: 0.0, right: 0.0)
         return view
     }()
 
-    let accountView: DetailsTriangularedView = UIFactory.default.createAccountView()
+    let stashAccountView = UIFactory.default.createAccountView()
 
-    let amountView: AmountInputView = {
-        let view = UIFactory().createAmountInputView(filled: true)
-        view.isUserInteractionEnabled = false
-        return view
-    }()
+    let controllerAccountView = UIFactory.default.createAccountView()
 
-    let networkFeeConfirmView: NetworkFeeConfirmView = UIFactory().createNetworkFeeConfirmView()
+    let networkFeeConfirmView: NetworkFeeConfirmView = UIFactory.default.createNetworkFeeConfirmView()
 
     var locale = Locale.current {
         didSet {
@@ -44,85 +37,41 @@ final class ControllerAccountConfirmationLayout: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func bind(confirmationViewModel: StakingUnbondConfirmViewModel) {
-        if let senderName = confirmationViewModel.senderName {
-            accountView.subtitleLabel?.lineBreakMode = .byTruncatingTail
-            accountView.subtitle = senderName
-        } else {
-            accountView.subtitleLabel?.lineBreakMode = .byTruncatingMiddle
-            accountView.subtitle = confirmationViewModel.senderAddress
-        }
-
-        let iconSize = 2.0 * accountView.iconRadius
-        accountView.iconImage = confirmationViewModel.senderIcon.imageWithFillColor(
-            R.color.colorWhite()!,
-            size: CGSize(width: iconSize, height: iconSize),
-            contentScale: UIScreen.main.scale
-        )
-
-        amountView.fieldText = confirmationViewModel.amount.value(for: locale)
-        setNeedsLayout()
-    }
-
-    func bind(feeViewModel: BalanceViewModelProtocol?) {
-        networkFeeConfirmView.networkFeeView.bind(viewModel: feeViewModel)
-        setNeedsLayout()
-    }
-
-    func bind(assetViewModel: AssetBalanceViewModelProtocol) {
-        amountView.priceText = assetViewModel.price
-
-        if let balance = assetViewModel.balance {
-            amountView.balanceText = R.string.localizable.stakingBondedFormat(
-                balance,
-                preferredLanguages: locale.rLanguages
-            )
-        } else {
-            amountView.balanceText = nil
-        }
-
-        amountView.assetIcon = assetViewModel.icon
-
-        amountView.symbol = assetViewModel.symbol.uppercased()
-
-        setNeedsLayout()
-    }
-
-    private func applyLocalization() {
-        accountView.title = R.string.localizable.commonAccount(preferredLanguages: locale.rLanguages)
-
-        amountView.title = R.string.localizable
-            .walletSendAmountTitle(preferredLanguages: locale.rLanguages)
-
-        networkFeeConfirmView.locale = locale
-
-        setNeedsLayout()
-    }
-
     private func setupLayout() {
-        addSubview(stackView)
-        stackView.snp.makeConstraints { make in
+        let separator = UIView.createSeparator(color: R.color.colorDarkGray())
+        addSubview(separator)
+        separator.snp.makeConstraints { make in
             make.top.equalTo(safeAreaLayoutGuide)
-            make.leading.trailing.equalToSuperview().inset(UIConstants.horizontalInset)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(UIConstants.separatorHeight)
         }
 
-        stackView.addArrangedSubview(accountView)
-        accountView.snp.makeConstraints { make in
-            make.width.equalTo(stackView)
-            make.height.equalTo(52)
+        addSubview(containerView)
+        containerView.snp.makeConstraints { make in
+            make.top.equalTo(separator.snp.bottom)
+            make.leading.bottom.trailing.equalToSuperview()
         }
 
-        stackView.setCustomSpacing(16.0, after: accountView)
-        stackView.addArrangedSubview(amountView)
-        amountView.snp.makeConstraints { make in
-            make.width.equalTo(stackView)
-            make.height.equalTo(72.0)
+        containerView.stackView.spacing = 16
+        containerView.stackView.addArrangedSubview(stashAccountView)
+        stashAccountView.snp.makeConstraints { make in
+            make.width.equalTo(self).offset(-2.0 * UIConstants.horizontalInset)
+            make.height.equalTo(52.0)
+        }
+
+        containerView.stackView.addArrangedSubview(controllerAccountView)
+        controllerAccountView.snp.makeConstraints { make in
+            make.width.equalTo(self).offset(-2.0 * UIConstants.horizontalInset)
+            make.height.equalTo(52.0)
         }
 
         addSubview(networkFeeConfirmView)
-
         networkFeeConfirmView.snp.makeConstraints { make in
             make.leading.bottom.trailing.equalToSuperview()
         }
+    }
+
+    private func applyLocalization() {
+        networkFeeConfirmView.locale = locale
     }
 }
