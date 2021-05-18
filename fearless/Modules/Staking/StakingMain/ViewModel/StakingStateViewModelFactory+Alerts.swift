4059cec6671e9ca9f4e8c43781abc8b879a1774c
Aleksandr Malina <ef1rspb@gmail.com>
2021-05-18 14:01:06 +0300

    Low stake alert

--- fearless/Modules/Staking/StakingMain/ViewModel/StakingStateViewModelFactory+Alerts.swift
+++ fearless/Modules/Staking/StakingMain/ViewModel/StakingStateViewModelFactory+Alerts.swift
@@ -5,21 +5,24 @@ extension StakingStateViewModelFactory {
     func stakingAlertsForNominatorState(_ state: NominatorState) -> [StakingAlert] {
         [
             findElectionAlert(commonData: state.commonData),
-            findRedeemUnbondedAlert(commonData: state.commonData, ledgerInfo: state.ledgerInfo)
+            findRedeemUnbondedAlert(commonData: state.commonData, ledgerInfo: state.ledgerInfo),
+            findLowStakeAlert(commonData: state.commonData, ledgerInfo: state.ledgerInfo)
         ].compactMap { $0 }
     }
 
     func stakingAlertsForValidatorState(_ state: ValidatorState) -> [StakingAlert] {
         [
             findElectionAlert(commonData: state.commonData),
-            findRedeemUnbondedAlert(commonData: state.commonData, ledgerInfo: state.ledgerInfo)
+            findRedeemUnbondedAlert(commonData: state.commonData, ledgerInfo: state.ledgerInfo),
+            findLowStakeAlert(commonData: state.commonData, ledgerInfo: state.ledgerInfo)
         ].compactMap { $0 }
     }
 
     func stakingAlertsForBondedState(_ state: BondedState) -> [StakingAlert] {
         [
             findElectionAlert(commonData: state.commonData),
-            findRedeemUnbondedAlert(commonData: state.commonData, ledgerInfo: state.ledgerInfo)
+            findRedeemUnbondedAlert(commonData: state.commonData, ledgerInfo: state.ledgerInfo),
+            findLowStakeAlert(commonData: state.commonData, ledgerInfo: state.ledgerInfo)
         ].compactMap { $0 }
     }
 
@@ -57,5 +60,33 @@ extension StakingStateViewModelFactory {
             redeemableAmount.value(for: locale)
         }
         return .redeemUnbonded(localizedString)
+    }
+
+    private func findLowStakeAlert(
+        commonData: StakingStateCommonData,
+        ledgerInfo: StakingLedger
+    ) -> StakingAlert? {
+        guard let minimalStake = commonData.minimalStake else {
+            return nil
+        }
+        guard
+            ledgerInfo.active >= minimalStake,
+            let chain = commonData.chain,
+            let minimalStakeDecimal = Decimal.fromSubstrateAmount(
+                minimalStake,
+                precision: chain.addressType.precision
+            ),
+            let minimalStakeAmount = balanceViewModelFactory?.amountFromValue(minimalStakeDecimal)
+        else {
+            return nil
+        }
+
+        let localizedString = LocalizableResource<String> { locale in
+            R.string.localizable.stakingInactiveCurrentMinimalStake(
+                minimalStakeAmount.value(for: locale),
+                preferredLanguages: locale.rLanguages
+            )
+        }
+        return .nominatorLowStake(localizedString)
     }
 }
