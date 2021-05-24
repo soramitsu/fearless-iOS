138639559cff6a786a324135293101bca4a8717b
Aleksandr Malina <ef1rspb@gmail.com>
2021-05-24 12:58:25 +0300

    Define cell

--- fearless/Modules/Staking/SelectValidators/SelectValidatorsViewController.swift
+++ fearless/Modules/Staking/SelectValidators/SelectValidatorsViewController.swift
@@ -1,13 +1,22 @@
 import UIKit
+import SoraFoundation
 
-final class SelectValidatorsViewController: UIViewController {
+final class SelectValidatorsViewController: UIViewController, ViewHolder {
     typealias RootViewType = SelectValidatorsViewLayout
 
     let presenter: SelectValidatorsPresenterProtocol
 
-    init(presenter: SelectValidatorsPresenterProtocol) {
+    private var cellStub: [SelectValidatorsCellViewModel] {
+        []
+    }
+
+    init(
+        presenter: SelectValidatorsPresenterProtocol,
+        localizationManager: LocalizationManagerProtocol? = nil
+    ) {
         self.presenter = presenter
         super.init(nibName: nil, bundle: nil)
+        self.localizationManager = localizationManager
     }
 
     @available(*, unavailable)
@@ -22,12 +31,35 @@ final class SelectValidatorsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        applyLocalization()
+        setupTable()
         presenter.setup()
+    }
+
+    private func setupTable() {
+        rootView.tableView.dataSource = self
+        // rootView.tableView.delegate = self
+        rootView.tableView.registerClassForCell(SelectValidatorsCell.self)
     }
 }
 
 extension SelectValidatorsViewController: SelectValidatorsViewProtocol {
     func applyLocalization() {
-        if isViewLoaded {}
+        if isViewLoaded {
+            title = "Select validators"
+        }
+    }
+}
+
+extension SelectValidatorsViewController: UITableViewDataSource {
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        cellStub.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithType(SelectValidatorsCell.self)!
+        let vm = cellStub[indexPath.row]
+        cell.bind(viewModel: vm)
+        return cell
     }
 }
