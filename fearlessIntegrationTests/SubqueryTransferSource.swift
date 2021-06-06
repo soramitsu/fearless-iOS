9086b2b08d3e8a2dc0e6a1f8bda103ae2c8d1b4b
Aleksandr Malina <ef1rspb@gmail.com>
2021-06-06 11:42:04 +0300

    Define SubqueryTransfersSource

--- fearlessIntegrationTests/SubqueryTransferSource.swift
+++ fearlessIntegrationTests/SubqueryTransferSource.swift
@@ -1,107 +1,11 @@
 import Foundation
-import RobinHood
-import FearlessUtils
-import BigInt
-import CommonWallet
 import XCTest
 @testable import fearless
-
-struct Transfer: Decodable {
-    let toId: String
-    let fromId: String
-    let amount: Decimal?
-    let id: String
-    let createdAd: String
-}
-
-final class SubqueryTransferSource {
-    typealias Model = [Transfer]
-
-    let address: String
-    let url: URL
-    let chain: Chain
-
-    init(
-        address: String,
-        url: URL,
-        chain: Chain
-    ) {
-        self.address = address
-        self.url = url
-        self.chain = chain
-    }
-}
-
-extension SubqueryTransferSource: SingleValueProviderSourceProtocol {
-
-    func fetchOperation() -> CompoundOperationWrapper<[Transfer]?> {
-        let requestFactory = BlockNetworkRequestFactory {
-            var request = URLRequest(url: self.url)
-
-            let params = """
-            {
-                query {
-                    transfers(
-                          last:100000000
-                          filter: {or: [{toId: {equalTo: \"\(self.address)\"}}, {fromId: {equalTo: \"\(self.address)\"}}]}
-                    ) {
-                      nodes {
-                        id
-                        amount
-                        fromId
-                        toId
-                        createdAt
-                      }
-                    }
-                }
-            }
-            """
-            let info = JSON.dictionaryValue(["query": JSON.stringValue(params)])
-            request.httpBody = try JSONEncoder().encode(info)
-            request.setValue(
-                HttpContentType.json.rawValue,
-                forHTTPHeaderField: HttpHeaderKey.contentType.rawValue
-            )
-            request.httpMethod = HttpMethod.post.rawValue
-            return request
-        }
-
-        let resultFactory = AnyNetworkResultFactory<[Transfer]?> { data in
-            let resultData = try JSONDecoder().decode(JSON.self, from: data)
-
-            let nodes = resultData.data?.query?.transfers?.nodes
-            return nodes?.arrayValue?
-                .compactMap { node -> Transfer? in
-                    guard
-                        let id = node.id?.stringValue,
-                        let fromId = node.fromId?.stringValue,
-                        let toId = node.toId?.stringValue,
-                        let createdAt = node.createdAt?.stringValue
-                    else { return nil }
-
-                    let amount = node.amount?.stringValue
-                        .map { BigUInt($0) }?
-                        .map { Decimal.fromSubstrateAmount($0, precision: self.chain.addressType.precision) }
-                        ?? Decimal(0)
-                    return Transfer(
-                        toId: toId,
-                        fromId: fromId,
-                        amount: amount,
-                        id: id,
-                        createdAd: createdAt
-                    )
-                }
-        }
-
-        let operation = NetworkOperation(requestFactory: requestFactory, resultFactory: resultFactory)
-        return CompoundOperationWrapper(targetOperation: operation)
-    }
-}
 
 class SubqueryTransferSourceTests: XCTestCase {
 
     func testConnection() {
-        let source = SubqueryTransferSource(
+        let source = SubqueryTransfersSource(
             address: "15cfSaBcTxNr8rV59cbhdMNCRagFr3GE6B3zZRsCp4QHHKPu",
             url: URL(string: "http://localhost:3000/")!,
             chain: .polkadot
