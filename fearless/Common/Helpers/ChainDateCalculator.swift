30cb068ac7ded8a1a03d5bcf2c36aab72cfe0de2
Russel <emkil.russel@gmail.com>
2021-06-16 14:05:43 +0300

    fix leasing period interval

--- fearless/Common/Helpers/ChainDateCalculator.swift
+++ fearless/Common/Helpers/ChainDateCalculator.swift
@@ -6,23 +6,29 @@ struct LeasingTimeInterval {
 }
 
 protocol ChainDateCalculatorProtocol {
-    func differenceBetweenLeasingPeriods(
-        firstPeriod: LeasingPeriod,
-        lastPeriod: LeasingPeriod,
+    func differenceBetweenLeasingSlots(
+        firstSlot: LeasingPeriod,
+        lastSlot: LeasingPeriod,
+        metadata: CrowdloanMetadata,
+        calendar: Calendar
+    ) -> LeasingTimeInterval?
+
+    func intervalTillSlot(
+        _ slot: LeasingPeriod,
         metadata: CrowdloanMetadata,
         calendar: Calendar
     ) -> LeasingTimeInterval?
 }
 
 final class ChainDateCalculator: ChainDateCalculatorProtocol {
-    func differenceBetweenLeasingPeriods(
-        firstPeriod: LeasingPeriod,
-        lastPeriod: LeasingPeriod,
+    func differenceBetweenLeasingSlots(
+        firstSlot: LeasingPeriod,
+        lastSlot: LeasingPeriod,
         metadata: CrowdloanMetadata,
         calendar: Calendar
     ) -> LeasingTimeInterval? {
-        let firstBlockNumber = firstPeriod * metadata.leasingPeriod
-        let lastBlockNumber = (lastPeriod + 1) * metadata.leasingPeriod
+        let firstBlockNumber = firstSlot * metadata.leasingPeriod
+        let lastBlockNumber = (lastSlot + 1) * metadata.leasingPeriod
 
         let leasingTimeInterval = firstBlockNumber.secondsTo(
             block: lastBlockNumber,
@@ -51,5 +57,24 @@ final class ChainDateCalculator: ChainDateCalculatorProtocol {
         }
 
         return LeasingTimeInterval(duration: max(leasingTimeInterval, 0), tillDate: lastPeriodDate)
+    }
+
+    func intervalTillSlot(
+        _ slot: LeasingPeriod,
+        metadata: CrowdloanMetadata,
+        calendar: Calendar
+    ) -> LeasingTimeInterval? {
+        let blockNumber = slot * metadata.leasingPeriod
+        let timeInterval = metadata.blockNumber.secondsTo(block: blockNumber, blockDuration: metadata.blockDuration)
+
+        guard let tillDate = calendar.date(
+            byAdding: .second,
+            value: Int(timeInterval),
+            to: Date()
+        ) else {
+            return nil
+        }
+
+        return LeasingTimeInterval(duration: max(timeInterval, 0), tillDate: tillDate)
     }
 }
