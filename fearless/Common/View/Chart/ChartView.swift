fbe3f13ae37fa89ee8efd4f1c069f7893e28697e
Aleksandr Malina <ef1rspb@gmail.com>
2021-06-16 18:14:45 +0300

    Set accent color

--- fearless/Common/View/Chart/ChartView.swift
+++ fearless/Common/View/Chart/ChartView.swift
@@ -65,7 +65,7 @@ extension ChartView: ChartViewProtocol {
         set.drawIconsEnabled = false
         set.drawValuesEnabled = false
         set.colors = [
-            UIColor(red: 0.36, green: 0.31, blue: 0.41, alpha: 1.0)
+            R.color.colorAccent()!
         ]
 
         xAxisFormmater.xAxisValues = data.xAxisValues
