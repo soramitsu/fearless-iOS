99949de44036c9da4adb20084ab812a094e7db31
Stas Litvinov <Stas.Litvinov@gmail.com>
2021-06-22 12:30:49 +0300

    fix build issue

--- fearless/Modules/Staking/SelectValidatorsFlow/SelectedValidatorList/View/SelectedValidatorListHeaderView.swift
+++ fearless/Modules/Staking/SelectValidatorsFlow/SelectedValidatorList/View/SelectedValidatorListHeaderView.swift
@@ -1,15 +1,6 @@
 import UIKit
 
 final class SelectedValidatorListHeaderView: CustomValidatorListHeaderView {
-    override init(reuseIdentifier: String?) {
-        super.init(reuseIdentifier: reuseIdentifier)
-    }
-
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
     func bind(viewModel: TitleWithSubtitleViewModel, shouldAlert: Bool) {
         bind(viewModel: viewModel)
 
