52637d887542fdc7d5d223eb8028ddeba8aaf973
Aleksandr Malina <ef1rspb@gmail.com>
2021-07-23 16:59:02 +0300

    Remove "Coming soon" module

--- fearless/Modules/CommingSoon/CommingSoonInteractor.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-final class CommingSoonInteractor {
-    weak var presenter: CommingSoonInteractorOutputProtocol!
-}
-
-extension CommingSoonInteractor: CommingSoonInteractorInputProtocol {}
