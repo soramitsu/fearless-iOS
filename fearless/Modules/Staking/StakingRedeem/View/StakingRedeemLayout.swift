e869dfcbd775150f10d198f0c4e6c1895d85f7f2
Aleksandr Malina <ef1rspb@gmail.com>
2021-07-23 17:01:34 +0300

    FLW-917 Redeem and Unbond Confirmation window: fix tooltip position

--- fearless/Modules/Staking/StakingRedeem/View/StakingRedeemLayout.swift
+++ fearless/Modules/Staking/StakingRedeem/View/StakingRedeemLayout.swift
@@ -144,6 +144,7 @@ final class StakingRedeemLayout: UIView {
 
         let hintView = HintView()
 
+        stackView.setCustomSpacing(16, after: amountView)
         stackView.addArrangedSubview(hintView)
         hintView.snp.makeConstraints { make in
             make.width.equalTo(stackView)
--- fearless/Modules/Staking/StakingUnbondConfirm/View/StakingUnbondConfirmLayout.swift
+++ fearless/Modules/Staking/StakingUnbondConfirm/View/StakingUnbondConfirmLayout.swift
@@ -144,6 +144,7 @@ final class StakingUnbondConfirmLayout: UIView {
 
         let hintView = HintView()
 
+        stackView.setCustomSpacing(16, after: amountView)
         stackView.addArrangedSubview(hintView)
         hintView.snp.makeConstraints { make in
             make.width.equalTo(stackView)
