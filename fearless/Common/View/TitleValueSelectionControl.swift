2b8bedfecb08dbb8db0c5d2b522d44d415c0f638
Russel <emkil.russel@gmail.com>
2021-07-27 01:28:12 +0300

    refactor title selection control

--- fearless/Common/View/TitleValueSelectionControl.swift
+++ fearless/Common/View/TitleValueSelectionControl.swift
@@ -1,7 +1,7 @@
 import UIKit
 import SoraUI
 
-final class TitleValueSelectionControl: BackgroundedContentControl {
+final class TitleValueSelectionView: UIView {
     let iconView: UIImageView = {
         let view = UIImageView()
         view.contentMode = .scaleAspectFit
@@ -32,12 +32,6 @@ final class TitleValueSelectionControl: BackgroundedContentControl {
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        let shapeView = ShapeView()
-        shapeView.isUserInteractionEnabled = false
-        shapeView.fillColor = .clear
-        shapeView.highlightedFillColor = R.color.colorCellSelection()!
-        backgroundView = shapeView
-
         setupLayout()
     }
 
@@ -46,55 +40,30 @@ final class TitleValueSelectionControl: BackgroundedContentControl {
         fatalError("init(coder:) has not been implemented")
     }
 
-    override func layoutSubviews() {
-        super.layoutSubviews()
-
-        contentView?.frame = CGRect(
-            x: bounds.minX + contentInsets.left,
-            y: bounds.minY + contentInsets.top,
-            width: max(bounds.width - contentInsets.left - contentInsets.right, 0),
-            height: max(bounds.height - contentInsets.top - contentInsets.bottom, 0)
-        )
-    }
-
-    override var intrinsicContentSize: CGSize {
-        CGSize(
-            width: UIView.noIntrinsicMetric,
-            height: 48.0
-        )
-    }
-
     private func setupLayout() {
-        let baseView = UIView()
-        baseView.isUserInteractionEnabled = false
-
-        baseView.addSubview(iconView)
+        addSubview(iconView)
         iconView.snp.makeConstraints { make in
             make.leading.centerY.equalToSuperview()
             make.size.equalTo(24.0)
         }
 
-        baseView.addSubview(titleLabel)
+        addSubview(titleLabel)
         titleLabel.snp.makeConstraints { make in
             make.leading.equalTo(iconView.snp.trailing).offset(12.0)
             make.centerY.equalToSuperview()
         }
 
-        baseView.addSubview(arrowIconView)
+        addSubview(arrowIconView)
         arrowIconView.snp.makeConstraints { make in
             make.trailing.centerY.equalToSuperview()
             make.size.equalTo(24.0)
         }
 
-        baseView.addSubview(detailsLabel)
+        addSubview(detailsLabel)
         detailsLabel.snp.makeConstraints { make in
             make.trailing.equalTo(arrowIconView.snp.leading).offset(-8.0)
             make.centerY.equalToSuperview()
             make.leading.greaterThanOrEqualTo(titleLabel.snp.trailing).offset(8.0)
         }
-
-        contentView = baseView
-
-        baseView.autoresizingMask = [.flexibleWidth]
     }
 }
---
+++ fearless/Modules/Staking/SelectValidatorsFlow/SelectValidatorsStart/SelectValidatorsViewLayout.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+final class SelectValidatorsViewLayout: UIView {
+    let contentView: ScrollableContainerView = {
+        let view = ScrollableContainerView()
+        view.stackView.isLayoutMarginsRelativeArrangement = true
+        view.stackView.layoutMargins = UIEdgeInsets(top: 8.0, left: 0.0, bottom: 0.0, right: 0.0)
+        return view
+    }()
+
+    var stackView: UIStackView {
+        contentView.stackView
+    }
+
+    let algoSectionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .h4Title
+        label.textColor = R.color.colorWhite()
+        return label
+    }()
+
+    let algoDetailsLabel: UILabel = {
+        let label = UILabel()
+        label.font = .p2Paragraph
+        label.textColor = R.color.colorLightGray()
+        return label
+    }()
+
+    let customValidatorsCell: RowView<TitleValueSelectionView> = {
+        let view = UIFactory.default.createTitleValueSelectionView()
+        view.iconView.image = R.image.iconList()
+        return RowView(contentView: view, preferredHeight: 48.0)
+    }()
+
+    let customValidatorsSectionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .h4Title
+        label.textColor = R.color.colorWhite()
+        return label
+    }()
+
+    let customValidatorsDetailsLabel: UILabel = {
+        let label = UILabel()
+        label.font = .p2Paragraph
+        label.textColor = R.color.colorLightGray()
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        backgroundColor = R.color.colorBlack()
+
+        setupLayout()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupLayout() {
+        addSubview(contentView)
+        contentView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide)
+            make.bottom.leading.trailing.equalToSuperview()
+        }
+    }
+}
