9768dcbf3cbf3c06bcb4e463485f8ace060bad34
Stas Litvinov <Stas.Litvinov@gmail.com>
2021-07-28 12:25:28 +0300

    fix missing localization on pending reward confirmation screen

--- fearless/Modules/Staking/StakingPayoutConfirmation/StakingPayoutConfirmationViewLayout.swift
+++ fearless/Modules/Staking/StakingPayoutConfirmation/StakingPayoutConfirmationViewLayout.swift
@@ -16,6 +16,14 @@ final class StakingPayoutConfirmationViewLayout: UIView {
 
     let networkFeeConfirmView: NetworkFeeConfirmView = UIFactory().createNetworkFeeConfirmView()
 
+    var locale = Locale.current {
+        didSet {
+            if locale != oldValue {
+                applyLocalization()
+            }
+        }
+    }
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupLayout()
@@ -46,5 +54,10 @@ final class StakingPayoutConfirmationViewLayout: UIView {
         networkFeeConfirmView.snp.makeConstraints { make in
             make.leading.bottom.trailing.equalToSuperview()
         }
+    }
+
+    private func applyLocalization() {
+        networkFeeConfirmView.locale = locale
+        setNeedsLayout()
     }
 }
