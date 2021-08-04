67b26958e70116b055a0434cb3d19836e7ee0fb0
Aleksandr Malina <ef1rspb@gmail.com>
2021-08-04 14:26:41 +0300

    Get rid of collectionView

--- fearless/Modules/Staking/Analytics/Common/AnalyticsPeriodView/AnalyticsPeriodView.swift
+++ fearless/Modules/Staking/Analytics/Common/AnalyticsPeriodView/AnalyticsPeriodView.swift
@@ -8,22 +8,30 @@ final class AnalyticsPeriodView: UIView {
     weak var delegate: AnalyticsPeriodViewDelegate?
 
     private var periods: [AnalyticsPeriod] = []
+    private var selectedPeriod: AnalyticsPeriod?
 
-    let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.estimatedItemSize = .init(width: 83, height: 24)
-        layout.minimumInteritemSpacing = 8
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.showsHorizontalScrollIndicator = false
-        collectionView.backgroundColor = .clear
-        return collectionView
+    private let buttonsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.spacing = 8
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        return stackView
     }()
+
+//    let collectionView: UICollectionView = {
+//        let layout = UICollectionViewFlowLayout()
+//        layout.scrollDirection = .horizontal
+//        layout.estimatedItemSize = .init(width: 83, height: 24)
+//        layout.minimumInteritemSpacing = 8
+//        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+//        collectionView.showsHorizontalScrollIndicator = false
+//        collectionView.backgroundColor = .clear
+//        return collectionView
+//    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupLayout()
-        setupCollection()
     }
 
     @available(*, unavailable)
@@ -32,120 +40,87 @@ final class AnalyticsPeriodView: UIView {
     }
 
     private func setupLayout() {
-        addSubview(collectionView)
-        collectionView.snp.makeConstraints { make in
+        addSubview(buttonsStackView)
+        buttonsStackView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
+            make.height.equalTo(24)
         }
-    }
-
-    private func setupCollection() {
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        collectionView.register(
-            AnalyticsPeriodCell.self,
-            forCellWithReuseIdentifier: AnalyticsPeriodCell.reuseIdentifier
-        )
     }
 
     func configure(periods: [AnalyticsPeriod], selected: AnalyticsPeriod) {
         self.periods = periods
-        collectionView.reloadData()
-        collectionView.selectItem(
-            at: IndexPath(row: periods.firstIndex(of: selected) ?? 0, section: 0),
-            animated: false,
-            scrollPosition: .centeredHorizontally
-        )
-    }
-}
-
-extension AnalyticsPeriodView: UICollectionViewDataSource {
-    func collectionView(_: UICollectionView, numberOfItemsInSection _: Int) -> Int {
-        periods.count
-    }
-
-    func collectionView(
-        _ collectionView: UICollectionView,
-        cellForItemAt indexPath: IndexPath
-    ) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: AnalyticsPeriodCell.reuseIdentifier,
-            for: indexPath
-        ) as? AnalyticsPeriodCell else {
-            return UICollectionViewCell()
+        buttonsStackView.arrangedSubviews.forEach { view in
+            view.removeFromSuperview()
         }
-        cell.titleLabel.text = periods[indexPath.row].title(for: .current).uppercased()
-        return cell
+        let buttons = periods.map { AnalyticsPeriodButton(period: $0) }
+        buttons.forEach { buttonsStackView.addArrangedSubview($0) }
+        setNeedsLayout()
+        let selectedButton = buttons.first(where: { $0.period == selected })
+        selectedButton?.isSelected = true
     }
+
+    @objc
+    private func handlePeriodButton(button _: AnalyticsPeriodButton) {}
 }
 
-extension AnalyticsPeriodView: UICollectionViewDelegate {
-    func collectionView(_: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let period = periods[indexPath.row]
-        delegate?.didSelect(period: period)
-    }
-}
+// extension AnalyticsPeriodView: UICollectionViewDelegateFlowLayout {
+//    func collectionView(
+//        _ collectionView: UICollectionView,
+//        layout _: UICollectionViewLayout,
+//        insetForSectionAt _: Int
+//    ) -> UIEdgeInsets {
+//        let cellCount = periods.count
+//        let cellSpacing = 8.0
+//        let totalCellWidth = 67.0 * Double(cellCount)
+//        let totalSpacingWidth = cellSpacing * Double(cellCount - 1)
+//
+//        let collectionViewWidth = collectionView.bounds.width
+//        let leftInset = (collectionViewWidth - CGFloat(totalCellWidth + totalSpacingWidth)) / 2
+//        let rightInset = leftInset
+//
+//        return UIEdgeInsets(top: 0, left: leftInset, bottom: 0, right: rightInset)
+//    }
+// }
 
-extension AnalyticsPeriodView: UICollectionViewDelegateFlowLayout {
-    func collectionView(
-        _ collectionView: UICollectionView,
-        layout _: UICollectionViewLayout,
-        insetForSectionAt _: Int
-    ) -> UIEdgeInsets {
-        let cellCount = periods.count
-        let cellSpacing = 8.0
-        let totalCellWidth = 67.0 * Double(cellCount)
-        let totalSpacingWidth = cellSpacing * Double(cellCount - 1)
-
-        let collectionViewWidth = collectionView.bounds.width
-        let leftInset = (collectionViewWidth - CGFloat(totalCellWidth + totalSpacingWidth)) / 2
-        let rightInset = leftInset
-
-        return UIEdgeInsets(top: 0, left: leftInset, bottom: 0, right: rightInset)
-    }
-}
-
-private class AnalyticsPeriodCell: UICollectionViewCell {
+private class AnalyticsPeriodButton: UIButton {
     override var isSelected: Bool {
         didSet {
             if isSelected {
-                contentView.backgroundColor = R.color.colorWhite()?.withAlphaComponent(0.16)
-                titleLabel.textColor = R.color.colorWhite()
+                backgroundColor = R.color.colorWhite()?.withAlphaComponent(0.16)
+                titleLabel?.textColor = R.color.colorWhite()
             } else {
-                contentView.backgroundColor = .clear
-                titleLabel.textColor = R.color.colorTransparentText()
+                backgroundColor = .clear
+                titleLabel?.textColor = R.color.colorTransparentText()
             }
         }
     }
 
-    let titleLabel: UILabel = {
-        let label = UILabel()
-        label.font = .capsTitle
-        return label
-    }()
+    let period: AnalyticsPeriod
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupLayout()
-        setupBorder()
+    init(period: AnalyticsPeriod) {
+        self.period = period
+        super.init(frame: .zero)
+        titleLabel?.font = .capsTitle
+        setTitle(period.title(for: .current), for: .normal)
     }
 
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+//
+//    private func setupLayout() {
+//        contentView.addSubview(titleLabel)
+//        titleLabel.snp.makeConstraints { make in
+//            make.leading.trailing.equalToSuperview().inset(12)
+//            make.top.bottom.equalToSuperview().inset(5.5)
+//        }
+//    }
 
-    private func setupLayout() {
-        contentView.addSubview(titleLabel)
-        titleLabel.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(12)
-            make.top.bottom.equalToSuperview().inset(5.5)
-        }
-    }
-
-    private func setupBorder() {
-        contentView.layer.cornerRadius = 12
-        contentView.clipsToBounds = true
-        contentView.layer.borderWidth = 2
-        contentView.layer.borderColor = R.color.colorWhite()?.withAlphaComponent(0.16).cgColor
-    }
+//    private func setupBorder() {
+//        contentView.layer.cornerRadius = 12
+//        contentView.clipsToBounds = true
+//        contentView.layer.borderWidth = 2
+//        contentView.layer.borderColor = R.color.colorWhite()?.withAlphaComponent(0.16).cgColor
+//    }
 }
