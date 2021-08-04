ca6116ae0e8fe5ddb37b67cb821e65fcc68c576d
Aleksandr Malina <ef1rspb@gmail.com>
2021-08-04 12:30:22 +0300

    Fix scrollToPage vertical page

--- fearless/Common/Extension/UIKit/UIScrollView+ScrollToPage.swift
+++ fearless/Common/Extension/UIKit/UIScrollView+ScrollToPage.swift
@@ -4,7 +4,7 @@ extension UIScrollView {
     func scrollTo(horizontalPage: Int? = 0, verticalPage: Int? = 0, animated: Bool? = true) {
         var frame: CGRect = self.frame
         frame.origin.x = frame.size.width * CGFloat(horizontalPage ?? 0)
-        frame.origin.y = frame.size.width * CGFloat(verticalPage ?? 0)
+        frame.origin.y = frame.size.height * CGFloat(verticalPage ?? 0)
         scrollRectToVisible(frame, animated: animated ?? true)
     }
 }
