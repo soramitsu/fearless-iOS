5782b02a73b808d26bb0bd4006fbfe16e9bc9aec
Aleksandr Malina <ef1rspb@gmail.com>
2021-08-11 11:46:31 +0300

    Count total eras

--- fearless/Modules/Staking/Analytics/AnalyticsValidators/ViewModel/AnalyticsValidatorsViewModelFactory.swift
+++ fearless/Modules/Staking/Analytics/AnalyticsValidators/ViewModel/AnalyticsValidatorsViewModelFactory.swift
@@ -10,16 +10,24 @@ final class AnalyticsValidatorsViewModelFactory: AnalyticsValidatorsViewModelFac
         page: AnalyticsValidatorsPage
     ) -> LocalizableResource<AnalyticsValidatorsViewModel> {
         LocalizableResource { locale in
+            let totalEras = self.totalErasCount(eraValidatorInfos: eraValidatorInfos)
+
             let validators: [AnalyticsValidatorItemViewModel] = eraValidatorInfos.map { info in
                 let address = info.address
                 let icon = try? self.iconGenerator.generateFromAddress(address)
                 let validatorName = (identitiesByAddress?[address]?.displayName) ?? address
+                let progressText = self.progressDescription(
+                    page: page,
+                    validatorInfo: info,
+                    totalErasCount: totalEras,
+                    locale: locale
+                )
 
                 return .init(
                     icon: icon,
                     validatorName: validatorName,
                     progress: 0.29,
-                    progressText: "29% (25 eras)",
+                    progressText: progressText,
                     validatorAddress: address
                 )
             }
@@ -41,5 +49,36 @@ final class AnalyticsValidatorsViewModelFactory: AnalyticsValidatorsViewModelFac
         case .rewards:
             return "Rewards".uppercased()
         }
+    }
+
+    func totalErasCount(eraValidatorInfos: [SQEraValidatorInfo]) -> Int {
+        let distinctEras = Set<EraIndex>(eraValidatorInfos.map(\.era))
+        return distinctEras.count
+    }
+
+    func progressDescription(
+        page: AnalyticsValidatorsPage,
+        validatorInfo: SQEraValidatorInfo,
+        totalErasCount: Int,
+        locale: Locale
+    ) -> String {
+        switch page {
+        case .activity:
+            return progressDescriptionStake(
+                validatorInfo: validatorInfo,
+                totalErasCount: totalErasCount,
+                locale: locale
+            )
+        case .rewards:
+            return "0 KSM"
+        }
+    }
+
+    func progressDescriptionStake(
+        validatorInfo _: SQEraValidatorInfo,
+        totalErasCount: Int,
+        locale _: Locale
+    ) -> String {
+        "(\(totalErasCount) eras)"
     }
 }
