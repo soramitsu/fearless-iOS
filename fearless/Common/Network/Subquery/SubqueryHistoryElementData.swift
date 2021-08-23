b8876cbce6be795c881d41816d205514e83a5fef
Aleksandr Malina <ef1rspb@gmail.com>
2021-08-23 09:54:30 +0300

    Delete unused model

--- fearless/Common/Network/Subquery/SubqueryHistoryElementData.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import FearlessUtils
-
-struct SubqueryHistoryElementData {
-    let id: String
-    let timestamp: Int64
-    let address: AccountAddress
-    let reward: SubqueryRewardItemData?
-}
