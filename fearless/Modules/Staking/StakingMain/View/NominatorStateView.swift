fc95f1ae0a91b5e895f64dae2359dea9bc4134f9
Russel <emkil.russel@gmail.com>
2021-09-02 16:48:59 +0300

    fix build issue

--- fearless/Modules/Staking/StakingMain/View/NominatorStateView.swift
+++ fearless/Modules/Staking/StakingMain/View/NominatorStateView.swift
@@ -24,11 +24,6 @@ class NominatorStateView: StakingStateView, LocalizableViewProtocol {
         timer.delegate = self
     }
 
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
     func bind(viewModel: LocalizableResource<NominationViewModelProtocol>) {
         localizableViewModel = viewModel
 
