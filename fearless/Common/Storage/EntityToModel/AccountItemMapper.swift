0d98153ace69d099c4a975190a5ea0e82fa45a3b
Russel <emkil.russel@gmail.com>
2021-09-10 20:16:08 +0300

    add account item wrapper

---
+++ fearless/Common/Storage/EntityToModel/AccountItemMapper.swift
@@ -0,0 +1,43 @@
+import Foundation
+import RobinHood
+import CoreData
+import IrohaCrypto
+
+enum AccountItemMapperError: Error {
+    case notSupported
+}
+
+final class AccountItemMapper: CoreDataMapperProtocol {
+    let addressPrefix: UInt16
+    let addressFactory: SS58AddressFactoryProtocol
+
+    init(addressPrefix: UInt16, addressFactory: SS58AddressFactoryProtocol) {
+        self.addressPrefix = addressPrefix
+        self.addressFactory = addressFactory
+    }
+
+    typealias DataProviderModel = AccountItem
+    typealias CoreDataEntity = CDMetaAccount
+
+    var entityIdentifierFieldName: String { "metaId" }
+
+    func transform(entity: CDMetaAccount) throws -> AccountItem {
+        let address = try addressFactory.address(fromAccountId: entity.substrateAccountId!, type: addressPrefix)
+        let cryptoType = CryptoType(rawValue: UInt8(entity.substrateCryptoType))
+
+        return AccountItem(
+            address: address,
+            cryptoType: cryptoType!,
+            username: entity.name!,
+            publicKeyData: entity.substratePublicKey!
+        )
+    }
+
+    func populate(
+        entity: CoreDataEntity,
+        from model: DataProviderModel,
+        using context: NSManagedObjectContext
+    ) throws {
+        throw AccountItemMapperError.notSupportedыро
+    }
+}
