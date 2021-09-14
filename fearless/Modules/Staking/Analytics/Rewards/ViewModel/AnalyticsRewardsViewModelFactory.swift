02437c5ee9bb34352db6d85140234e22faced6c9
Aleksandr Malina <ef1rspb@gmail.com>
2021-09-14 12:12:10 +0300

    Refactor rewards viewModelFactory

--- fearless/Modules/Staking/Analytics/Rewards/ViewModel/AnalyticsRewardsViewModelFactory.swift
+++ fearless/Modules/Staking/Analytics/Rewards/ViewModel/AnalyticsRewardsViewModelFactory.swift
@@ -15,14 +15,9 @@ final class AnalyticsRewardsViewModelFactory: AnalyticsViewModelFactoryBase<Subq
         let dates = data.map(\.date)
         guard let startDate = dates.first, let endDate = dates.last else { return [] }
 
-        let count = period.chartBarsCount(startDate: startDate, endDate: endDate, calendar: calendar)
-        let formatter = dateFormatter(period: period, for: locale)
-
         let dateComponents: Set<Calendar.Component> = {
             switch period {
-            case .month:
-                return [.year, .month, .day]
-            case .week:
+            case .month, .week:
                 return [.year, .month, .day]
             case .year, .all:
                 return [.year, .month]
@@ -30,43 +25,50 @@ final class AnalyticsRewardsViewModelFactory: AnalyticsViewModelFactoryBase<Subq
         }()
         let groupedByDate = data
             .groupedBy(dateComponents: dateComponents, calendar: calendar)
-        let sortedByDate: [(Date, [SubqueryRewardItemData])] = groupedByDate.keys
-            .map { (key: Date) in
-                (key, groupedByDate[key]!)
-            }
+        let sortedRewardsByDate: [(Date, [SubqueryRewardItemData])] = groupedByDate.keys
+            .map { (key: Date) in (key, groupedByDate[key]!) }
             .sorted(by: { $0.0 < $1.0 })
 
         let timestampInterval = period.timestampInterval(startDate: startDate, endDate: endDate, calendar: calendar)
-        let distance = timestampInterval.1 - timestampInterval.0
-        return (0 ..< count).map { index in
-            let portion = distance / Int64(count)
-            let cur: Int64 = timestampInterval.0 + Int64(index) * portion
-            let date = Date(timeIntervalSince1970: TimeInterval(cur))
-            let first = sortedByDate.last(where: { $0.0.timeIntervalSince(date) < TimeInterval(portion) })
-            if let tuple = first {
-                let dateTitle = formatter.string(from: tuple.0)
-                let yValue = tuple.1.map(\.amount)
-                    .compactMap { amount in
-                        Decimal.fromSubstrateAmount(
-                            amount,
-                            precision: chain.addressType.precision
-                        )
-                    }
-                    .reduce(0.0, +)
+        let chartBarsCount = period.chartBarsCount(startDate: startDate, endDate: endDate, calendar: calendar)
+        let formatter = dateFormatter(period: period, for: locale)
+        let timestampDistance = timestampInterval.1 - timestampInterval.0
 
-                return AnalyticsSelectedChartData(
-                    yValue: yValue,
-                    dateTitle: dateTitle,
-                    sections: createSections(rewardsData: tuple.1, locale: locale)
-                )
-            } else {
-                return AnalyticsSelectedChartData(
-                    yValue: 0,
-                    dateTitle: "",
-                    sections: []
-                )
-            }
+        return (0 ..< chartBarsCount).map { index in
+            let indexOfAccumulatedBar = timestampDistance / Int64(chartBarsCount)
+            let timestampOfAccumulatedBar: Int64 = timestampInterval.0 + Int64(index) * indexOfAccumulatedBar
+            let dateRepresentingAccumulatedBar = Date(timeIntervalSince1970: TimeInterval(timestampOfAccumulatedBar))
+            let rewardsByDate = sortedRewardsByDate
+                .last(where: { rewardsByDate in
+                    let timeIntervalOfAccumulatedBar = rewardsByDate.0.timeIntervalSince(dateRepresentingAccumulatedBar)
+                    return timeIntervalOfAccumulatedBar < TimeInterval(indexOfAccumulatedBar)
+                })
+            return createSelectedChartData(rewardsByDate: rewardsByDate, dateFormatter: formatter, locale: locale)
         }
+    }
+
+    private func createSelectedChartData(
+        rewardsByDate: (Date, [SubqueryRewardItemData])?,
+        dateFormatter: DateFormatter,
+        locale: Locale
+    ) -> AnalyticsSelectedChartData {
+        guard let rewardsByDate = rewardsByDate else {
+            return AnalyticsSelectedChartData(
+                yValue: 0,
+                dateTitle: "",
+                sections: []
+            )
+        }
+
+        let yValue = rewardsByDate.1
+            .compactMap { Decimal.fromSubstrateAmount($0.amount, precision: chain.addressType.precision) }
+            .reduce(0.0, +)
+
+        return AnalyticsSelectedChartData(
+            yValue: yValue,
+            dateTitle: dateFormatter.string(from: rewardsByDate.0),
+            sections: createSections(rewardsData: rewardsByDate.1, locale: locale)
+        )
     }
 }
 
