7d3cfd4edb860e6961f53c9115b5161f019be651
Russel <emkil.russel@gmail.com>
2021-09-16 03:00:21 +0300

    add error handling for web socket messages

---
+++ fearless/Common/Network/JSONRPC/JSONRPCError+Presentable.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+extension JSONRPCError: ErrorContentConvertible {
+    func toErrorContent(for locale: Locale?) -> ErrorContent {
+        let title: String
+        let details: String
+
+        if let data = data {
+            title = message
+            details = "\(data) (code \(code))"
+        } else {
+            title = R.string.localizable.operationErrorTitle(preferredLanguages: locale?.rLanguages)
+            details = "\(message) (code \(code))"
+        }
+
+        return ErrorContent(title: title, message: details)
+    }
+}
