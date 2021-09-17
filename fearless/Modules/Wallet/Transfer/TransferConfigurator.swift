b1f2d30c0c00cdddd295c6958373a86606bdbb5f
Stas Litvinov <Stas.Litvinov@gmail.com>
2021-09-17 19:07:12 +0300

    change keyboard mode from "push" to "overlay"

--- fearless/Modules/Wallet/Transfer/TransferConfigurator.swift
+++ fearless/Modules/Wallet/Transfer/TransferConfigurator.swift
@@ -125,5 +125,6 @@ final class TransferConfigurator {
             .with(accessoryViewFactory: WalletSingleActionAccessoryFactory.self)
             .with(operationDefinitionFactory: definitionFactory)
             .with(resultValidator: TransferValidator())
+            .with(accessoryOverlayMode: .overlay)
     }
 }
