6c3956170703ed54053ec8094bdc1ee2931e08c3
Aleksandr Malina <ef1rspb@gmail.com>
2021-10-04 10:38:37 +0300

    FLW-1327 Fix Analytics, Validators tab: pie chart is updated several times

--- fearless/Modules/Staking/Analytics/Validators/ViewModel/AnalyticsValidatorItemViewModel.swift
+++ fearless/Modules/Staking/Analytics/Validators/ViewModel/AnalyticsValidatorItemViewModel.swift
@@ -10,3 +10,15 @@ struct AnalyticsValidatorItemViewModel {
     let progressFullDescription: String
     let validatorAddress: AccountAddress
 }
+
+extension AnalyticsValidatorItemViewModel: Equatable {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.validatorName == rhs.validatorName
+            && lhs.amount == rhs.amount
+            && lhs.progressPercents == rhs.progressPercents
+            && lhs.mainValueText == rhs.mainValueText
+            && lhs.secondaryValueText == rhs.secondaryValueText
+            && lhs.progressFullDescription == rhs.progressFullDescription
+            && lhs.validatorAddress == rhs.validatorAddress
+    }
+}
