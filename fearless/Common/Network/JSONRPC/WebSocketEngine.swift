5e92b5bea86483285f2f5bf10202ff31d518c0e1
Alex Oakley <oakley@soramitsu.co.jp>
2021-10-16 16:12:22 +0300

    Fixed WebSockets response unexpected order support (#415)

--- fearless/Common/Network/JSONRPC/WebSocketEngine.swift
+++ fearless/Common/Network/JSONRPC/WebSocketEngine.swift
@@ -63,6 +63,7 @@ final class WebSocketEngine {
     private(set) var pendingRequests: [JSONRPCRequest] = []
     private(set) var inProgressRequests: [UInt16: JSONRPCRequest] = [:]
     private(set) var subscriptions: [UInt16: JSONRPCSubscribing] = [:]
+    private(set) var unknownResponsesByRemoteId: [String: [Data]] = [:]
 
     weak var delegate: WebSocketEngineDelegate?
 
@@ -416,7 +417,15 @@ extension WebSocketEngine {
         do {
             let response = try jsonDecoder.decode(JSONRPCData<String>.self, from: data)
             subscriptions[identifier]?.remoteId = response.result
-
+            
+            if let postponed = unknownResponsesByRemoteId[response.result] {
+                for data in postponed {
+                    try processSubscriptionUpdate(data)
+                }
+                
+                unknownResponsesByRemoteId[response.result] = nil
+            }
+            
             logger.debug("Did receive subscription id: \(response.result)")
         } catch {
             processSubscriptionError(identifier, error: error, shouldUnsubscribe: true)
@@ -445,6 +454,11 @@ extension WebSocketEngine {
             }
         } else {
             logger.warning("No handler for subscription: \(remoteId)")
+            if unknownResponsesByRemoteId[remoteId] == nil {
+                unknownResponsesByRemoteId[remoteId] = []
+            }
+            
+            unknownResponsesByRemoteId[remoteId]?.append(data)
         }
     }
 
