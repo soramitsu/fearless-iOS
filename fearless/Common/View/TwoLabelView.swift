e26909c252a4b257b9fa69409422a52c2ccd0514
Denis Lebedko <d.lebedko@soramitsu.co.jp>
2021-12-06 11:20:12 +0700

    (FLW-1396) remove copyright

--- fearless/Common/View/TwoLabelView.swift
+++ fearless/Common/View/TwoLabelView.swift
@@ -1,11 +1,3 @@
-//
-//  TriangularedTwoLabelView.swift
-//  fearless
-//
-//  Created by Денис Лебедько on 29.11.2021.
-//  Copyright © 2021 Soramitsu. All rights reserved.
-//
-
 import UIKit
 
 class TwoVerticalLabelView: UIView {
