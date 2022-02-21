d32d700f1e300cd63835e8b60d0ab2024e3e9ad2
Alex Lebedko <lebedko@soramitsu.co.jp>
2022-02-21 17:42:37 +0700

    migration ready

--- fearless/Common/Migration/MultiassetV2MigrationPolicy.swift
+++ fearless/Common/Migration/MultiassetV2MigrationPolicy.swift
@@ -3,58 +3,53 @@ import CoreData
 import IrohaCrypto
 
 class MultiassetV2MigrationPolicy: NSEntityMigrationPolicy {
-    override func end(_ mapping: NSEntityMapping, manager: NSMigrationManager) throws {
+    override func createDestinationInstances(
+        forSource oldMetaAccount: NSManagedObject,
+        in mapping: NSEntityMapping,
+        manager: NSMigrationManager
+    ) throws {
         guard let keystoreMigrator = manager
             .userInfo?[UserStorageMigratorKeys.keystoreMigrator] as? KeystoreMigrating else {
             fatalError("No keystore migrator found in context")
         }
 
-        let entityName = "CDMetaAccount"
-        let request = NSFetchRequest<NSManagedObject>(entityName: entityName)
-        let context = manager.sourceContext
-        let results = try context.fetch(request)
+        try super.createDestinationInstances(forSource: oldMetaAccount, in: mapping, manager: manager)
 
-        for metaAccount in results {
-            guard let address = metaAccount.value(forKey: "ethereumAddress") as? String,
-                  let metaId = metaAccount.value(forKey: "metaId") as? String else {
-                continue
-            }
-
-            if let ethereumPublicKey = try migrateKeystore(
-                for: address,
-                metaId: metaId,
-                keystoreMigrator: keystoreMigrator
-            ) {
-                let rawPublicKey = ethereumPublicKey.rawData()
-                metaAccount.setValue(rawPublicKey, forKey: "ethereumPublicKey")
-
-                let ethereumAddress = try rawPublicKey.ethereumAddressFromPublicKey()
-                metaAccount.setValue(ethereumAddress.toHex(), forKey: "ethereumAddress")
-            }
+        guard let metaAccount = manager.destinationInstances(
+            forEntityMappingName: mapping.name,
+            sourceInstances: [oldMetaAccount]
+        ).first else {
+            return
         }
-        try super.end(mapping, manager: manager)
+
+        guard let address = metaAccount.value(forKey: "ethereumAddress") as? String,
+              let metaId = metaAccount.value(forKey: "metaId") as? String else {
+            return
+        }
+
+        if let ethereumPublicKey = try migrateKeystore(
+            for: address,
+            metaId: metaId,
+            keystoreMigrator: keystoreMigrator
+        ) {
+            let rawPublicKey = ethereumPublicKey.rawData()
+            metaAccount.setValue(rawPublicKey, forKey: "ethereumPublicKey")
+
+            let ethereumAddress = try rawPublicKey.ethereumAddressFromPublicKey()
+            metaAccount.setValue(ethereumAddress.toHex(), forKey: "ethereumAddress")
+        }
     }
 
     private func migrateKeystore(
-        for sourceAddress: AccountAddress,
+        for _: AccountAddress,
         metaId: String,
         keystoreMigrator: KeystoreMigrating
     ) throws -> IRPublicKeyProtocol? {
         var publicKey: IRPublicKeyProtocol?
-        
-        let ethSecretKeyTag = KeystoreTagV2.ethereumSecretKeyTagForMetaId(metaId)
-        let ethereumSeedTag = KeystoreTagV2.ethereumSeedTagForMetaId(metaId)
 
-        let secretKey = try keystoreMigrator.fetchKey(for: ethSecretKeyTag)
-        
-        
-        let oldEntropyTag = KeystoreTag.entropyTagForAddress(sourceAddress)
-        if let entropy = keystoreMigrator.fetchKey(for: oldEntropyTag) {
-            let newEntropyTag = KeystoreTagV2.entropyTagForMetaId(metaId)
+        let entropyTag = KeystoreTagV2.entropyTagForMetaId(metaId)
 
-            keystoreMigrator.deleteKey(for: oldEntropyTag)
-            keystoreMigrator.save(key: entropy, for: newEntropyTag)
-
+        if let entropy = keystoreMigrator.fetchKey(for: entropyTag) {
             let ethereumDPString = DerivationPathConstants.defaultEthereum
             let secrets = try EthereumAccountImportWrapper().importEntropy(
                 entropy,
@@ -73,30 +68,6 @@ class MultiassetV2MigrationPolicy: NSEntityMigrationPolicy {
             }
 
             publicKey = secrets.keypair.publicKey()
-        }
-
-        let oldSeedTag = KeystoreTag.seedTagForAddress(sourceAddress)
-        if let seed = keystoreMigrator.fetchKey(for: oldSeedTag) {
-            let newSeedTag = KeystoreTagV2.substrateSeedTagForMetaId(metaId)
-
-            keystoreMigrator.deleteKey(for: oldSeedTag)
-            keystoreMigrator.save(key: seed, for: newSeedTag)
-        }
-
-        let oldSecretKeyTag = KeystoreTag.secretKeyTagForAddress(sourceAddress)
-        if let secretKey = keystoreMigrator.fetchKey(for: oldSecretKeyTag) {
-            let newSecretKeyTag = KeystoreTagV2.substrateSecretKeyTagForMetaId(metaId)
-
-            keystoreMigrator.deleteKey(for: oldSecretKeyTag)
-            keystoreMigrator.save(key: secretKey, for: newSecretKeyTag)
-        }
-
-        let oldDPTag = KeystoreTag.deriviationTagForAddress(sourceAddress)
-        if let derivationPath = keystoreMigrator.fetchKey(for: oldDPTag) {
-            keystoreMigrator.deleteKey(for: oldDPTag)
-
-            let newDPTag = KeystoreTagV2.substrateDerivationTagForMetaId(metaId)
-            keystoreMigrator.save(key: derivationPath, for: newDPTag)
         }
 
         return publicKey
