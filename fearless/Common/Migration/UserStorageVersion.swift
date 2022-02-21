f5189b1891899ba6461c6254155dd4dea35d3d26
Alex Lebedko <lebedko@soramitsu.co.jp>
2022-02-21 16:48:27 +0700

    migration ethereum keystore incomplete

--- fearless/Common/Migration/UserStorageVersion.swift
+++ fearless/Common/Migration/UserStorageVersion.swift
@@ -3,6 +3,7 @@ import Foundation
 enum UserStorageVersion: String, CaseIterable {
     case version1 = "UserDataModel"
     case version2 = "MultiassetUserDataModel"
+    case version3 = "MultiassetUserDataModel_v2"
 
     static var current: UserStorageVersion {
         guard let currentVersion = allCases.last else {
@@ -17,6 +18,8 @@ enum UserStorageVersion: String, CaseIterable {
         case .version1:
             return .version2
         case .version2:
+            return .version3
+        case .version3:
             return nil
         }
     }
