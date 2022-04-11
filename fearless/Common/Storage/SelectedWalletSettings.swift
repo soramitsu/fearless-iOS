d28ab1148f5de19db43fe014cb1b42e3db4ac8fe
Alex Lebedko <lebedko@soramitsu.co.jp>
2022-04-11 18:14:56 +0700

    code review fix

--- fearless/Common/Storage/SelectedWalletSettings.swift
+++ fearless/Common/Storage/SelectedWalletSettings.swift
@@ -90,10 +90,10 @@ final class SelectedWalletSettings: PersistentValueSettings<MetaAccountModel> {
 
         dependencies.forEach { saveOperation.addDependency($0) }
 
-        saveOperation.completionBlock = {
+        saveOperation.completionBlock = { [weak self] in
             do {
                 _ = try saveOperation.extractNoCancellableResultData()
-                self.internalValue = value
+                self?.internalValue = value
                 completionClosure(.success(value))
             } catch {
                 completionClosure(.failure(error))
