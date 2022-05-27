8067075146f71034e756d692f86250c9cd65af6b
Alex Lebedko <lebedko@soramitsu.co.jp>
2022-05-27 18:17:26 +0700

    app version comparator fix

--- fearless/Common/Extension/String+VersionComparsion.swift
+++ fearless/Common/Extension/String+VersionComparsion.swift
@@ -14,11 +14,11 @@ extension String {
         let comparableComponentsCount = min(currentVersionComponents.count, minimalVersionComponents.count)
 
         for index in 0 ... comparableComponentsCount - 1 {
-            let currentVersionComponent = currentVersionComponents[index]
-            let minimalVersionComponent = minimalVersionComponents[index]
-
-            if currentVersionComponent < minimalVersionComponent {
-                return true
+            if let currentVersionComponent = Int(currentVersionComponents[index]),
+               let minimalVersionComponent = Int(minimalVersionComponents[index]) {
+                return currentVersionComponent < minimalVersionComponent
+            } else {
+                return false
             }
         }
 
