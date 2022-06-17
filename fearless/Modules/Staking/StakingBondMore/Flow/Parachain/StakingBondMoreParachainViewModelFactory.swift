472a353e81930c3a4f988049ed927450dcd7479a
Alex Lebedko <lebedko@soramitsu.co.jp>
2022-06-17 15:32:43 +0700

    stake less, revoke tokens

---
+++ fearless/Modules/Staking/StakingBondMore/Flow/Parachain/StakingBondMoreParachainViewModelFactory.swift
@@ -0,0 +1,9 @@
+//
+//  StakingBondMoreParachainViewModelFactory.swift
+//  fearless
+//
+//  Created by alex on 16.06.2022.
+//  Copyright © 2022 Soramitsu. All rights reserved.
+//
+
+import Foundation
