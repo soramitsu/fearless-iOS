299d200e7b1a0c80e6918c12cd27cda39266f8a8
Alex Lebedko <lebedko@soramitsu.co.jp>
2022-10-26 17:30:39 +0700

    Update fearless/Modules/Staking/SelectValidatorsFlow/SelectValidatorsConfirm/Flow/Parachain/SelectValidatorsConfirmParachainViewModelFactory.swift
    Co-authored-by: Radmir Dzhurabaev <52320354+DRadmir@users.noreply.github.com>

--- fearless/Modules/Staking/SelectValidatorsFlow/SelectValidatorsConfirm/Flow/Parachain/SelectValidatorsConfirmParachainViewModelFactory.swift
+++ fearless/Modules/Staking/SelectValidatorsFlow/SelectValidatorsConfirm/Flow/Parachain/SelectValidatorsConfirmParachainViewModelFactory.swift
@@ -150,7 +150,7 @@ extension SelectValidatorsConfirmParachainViewModelFactory: SelectValidatorsConf
             let stakedAmountAttributedString = NSMutableAttributedString(string: stakedString)
             stakedAmountAttributedString.addAttribute(
                 NSAttributedString.Key.foregroundColor,
-                value: R.color.colorWhite(),
+                value: R.color.colorWhite() as Any,
                 range: (stakedString as NSString).range(of: amountString)
             )
 
