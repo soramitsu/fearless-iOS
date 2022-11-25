7b5b9995fcee2b550e700c45f54820a45e6f52e4
Radmir Dzhurabaev <dzhurabaev@soramitsu.co.jp>
2022-11-25 17:31:13 +0800

    FLW-2674 Send. There is not warning icon when you are sending to scam address

--- fearless/Modules/Send/SendPresenter.swift
+++ fearless/Modules/Send/SendPresenter.swift
@@ -203,6 +203,7 @@ extension SendPresenter: SendViewOutput {
 
 extension SendPresenter: SendInteractorOutput {
     func didReceive(scamInfo: ScamInfo?) {
+        self.scamInfo = scamInfo
         view?.didReceive(scamInfo: scamInfo)
     }
 
