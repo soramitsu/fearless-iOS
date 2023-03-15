7c17364027eb943b1e816b3e546854fab192fdfb
Denis Lebedko <Viscount88@gmail.com>
2023-03-15 15:17:54 +0700

    add sora card widget to settings

--- fearless/Modules/Profile/ViewModel/ProfileViewModelFactory.swift
+++ fearless/Modules/Profile/ViewModel/ProfileViewModelFactory.swift
@@ -113,7 +113,7 @@ final class ProfileViewModelFactory: ProfileViewModelFactoryProtocol {
             case .accountList:
                 return createAccountListViewModel(for: locale)
             case .soraCard:
-                return nil
+                return createSoraCardViewModel(for: locale)
             case .changePincode:
                 return createChangePincode(for: locale)
             case .language:
