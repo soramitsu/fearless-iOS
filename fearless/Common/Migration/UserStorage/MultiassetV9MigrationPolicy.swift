dcbfebd7eb955f65a22f064465bfbb8bec404dce
Alex Lebedko <lebedko@soramitsu.co.jp>
2023-03-24 09:47:07 +0700

    migration fix

--- fearless/Common/Migration/UserStorage/MultiassetV9MigrationPolicy.swift
+++ fearless/Common/Migration/UserStorage/MultiassetV9MigrationPolicy.swift
@@ -8,11 +8,11 @@ class MultiassetV9MigrationPolicy: NSEntityMigrationPolicy {
         in mapping: NSEntityMapping,
         manager: NSMigrationManager
     ) throws {
+        try super.createDestinationInstances(forSource: wallet, in: mapping, manager: manager)\
+        
         guard let assetIdsEnabled = wallet.value(forKey: "assetIdsEnabled") as? [String] else {
             return
         }
-
-        try super.createDestinationInstances(forSource: wallet, in: mapping, manager: manager)
 
         guard let updatedWallet = manager.destinationInstances(
             forEntityMappingName: mapping.name,
