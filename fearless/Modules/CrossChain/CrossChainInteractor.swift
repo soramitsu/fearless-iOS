2670a6cea6cc1ad51278f656b0fa43e50fac52f0
Radmir Dzhurabaev <dzhurabaev@soramitsu.co.jp>
2023-06-16 14:07:41 +0600

    fix for xc token

--- fearless/Modules/CrossChain/CrossChainInteractor.swift
+++ fearless/Modules/CrossChain/CrossChainInteractor.swift
@@ -137,7 +137,13 @@ final class CrossChainInteractor {
                     .chainAssets
                     .filter {
                         let symbol = $0.asset.symbol.lowercased()
-                        return availableOriginAsset?.contains(symbol) == true
+                        if availableOriginAsset?.contains(symbol) == true {
+                            return true
+                        } else if symbol.hasPrefix("xc") {
+                            let modifySymbol = String(symbol.dropFirst(2))
+                            return availableOriginAsset?.contains(modifySymbol) == true
+                        }
+                        return false
                     }
 
                 chainAssetFetching.fetch(
