6f0af32b00ea8ab2a19672b86491d35d5babce39
Alex Lebedko <lebedko@soramitsu.co.jp>
2023-06-26 15:10:25 +0700

    fix black blank screen instead of staking

--- fearless/Modules/Staking/StakingMain/StakingMainViewFactory.swift
+++ fearless/Modules/Staking/StakingMain/StakingMainViewFactory.swift
@@ -45,14 +45,12 @@ final class StakingMainViewFactory: StakingMainViewFactoryProtocol {
 
         // MARK: - Interactor
 
-        guard let interactor = createInteractor(
+        let interactor = createInteractor(
             state: sharedState,
             settings: settings,
             selectedAccount: selectedAccount,
             chainAsset: chainAsset
-        ) else {
-            return nil
-        }
+        )
 
         // MARK: - Router
 
@@ -112,7 +110,7 @@ final class StakingMainViewFactory: StakingMainViewFactoryProtocol {
         settings: SettingsManagerProtocol,
         selectedAccount: MetaAccountModel,
         chainAsset: ChainAsset
-    ) -> StakingMainInteractor? {
+    ) -> StakingMainInteractor {
         let chainRegistry = ChainRegistryFacade.sharedRegistry
 
         let operationManager = OperationManagerFacade.sharedManager
