7b0bc62ad88ed7b166c9a8a7853c0ff094bb6d36
Radmir Dzhurabaev <dzhurabaev@soramitsu.co.jp>
2023-08-04 15:44:00 +0600

    inactiveSessionDropTimeInSeconds revert

--- fearless/Common/Constants/UtilityConstants.swift
+++ fearless/Common/Constants/UtilityConstants.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 enum UtilityConstants {
-    #if F_DEV
-        static let inactiveSessionDropTimeInSeconds: TimeInterval = 180
-    #else
         static let inactiveSessionDropTimeInSeconds: TimeInterval = 1200
-    #endif
 }
