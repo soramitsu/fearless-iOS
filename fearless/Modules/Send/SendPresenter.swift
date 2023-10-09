6c2a1557b44f6fb701cdd188eb30d1e710191f9f
Radmir Dzhurabaev <dzhurabaev@soramitsu.co.jp>
2023-10-09 12:40:58 +0600

    new bokolo cash bridge address

--- fearless/Modules/Send/SendPresenter.swift
+++ fearless/Modules/Send/SendPresenter.swift
@@ -4,7 +4,7 @@ import BigInt
 import SSFUtils
 import SSFModels
 
-let bokoloCasheBridgeAddress = "cnSftmQkwkLoX2JEVDZ6xKScLDDiCHZoEZvgnUCrwZgrzEEpy"
+let bokoloCasheBridgeAddress = "cnRW3S6XXtQJtYDRdL7sF6o1PMWihqRrZ9R5KiXsNyJRsqBVW"
 let bokoloCashAssetCurrencyId = "0x00eacaea6599a04358fda986388ef0bb0c17a553ec819d5de2900c0af0862502"
 
 final class SendPresenter {
