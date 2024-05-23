9eef2741fcea1dd462fefd46588d46a775f4eca5
Radmir Dzhurabaev <dzhurabaev@soramitsu.co.jp>
2024-05-23 14:51:32 +0500

    your validators layout fixes

--- fearless/Modules/Staking/SelectValidatorsFlow/YourValidatorList/View/YourValidatorListViewLayout.swift
+++ fearless/Modules/Staking/SelectValidatorsFlow/YourValidatorList/View/YourValidatorListViewLayout.swift
@@ -1,13 +1,6 @@
 import UIKit
 
 final class YourValidatorListViewLayout: UIView {
-    let oversubscribedWarningView: HintView = {
-        let view = HintView()
-        view.isHidden = true
-        view.iconView.image = R.image.iconWarning()
-        return view
-    }()
-
     let tableView: UITableView = {
         let tableView = UITableView()
         tableView.tableFooterView = UIView()
@@ -47,21 +40,12 @@ final class YourValidatorListViewLayout: UIView {
     }
 
     private func setupLayout() {
-        let stackView = UIFactory.default.createVerticalStackView()
-        addSubview(stackView)
         addSubview(tableView)
         addSubview(changeValidatorsButton)
         addSubview(emptyView)
 
-        stackView.addArrangedSubview(oversubscribedWarningView)
-
-        stackView.snp.makeConstraints { make in
-            make.top.equalTo(safeAreaLayoutGuide)
-            make.leading.trailing.equalToSuperview().inset(16)
-        }
-
         tableView.snp.makeConstraints { make in
-            make.top.equalTo(stackView.snp.bottom)
+            make.top.equalTo(safeAreaLayoutGuide)
             make.leading.bottom.trailing.equalToSuperview()
         }
 
@@ -85,7 +69,6 @@ final class YourValidatorListViewLayout: UIView {
         changeValidatorsButton.imageWithTitleView?.title = R.string.localizable.yourValidatorsChangeValidatorsTitle(
             preferredLanguages: locale.rLanguages
         )
-        oversubscribedWarningView.titleLabel.text = R.string.localizable.stakingYourOversubscribedMessage(preferredLanguages: locale.rLanguages)
 
         let emptyViewModel = EmptyViewModel(
             title: R.string.localizable.stakingSetValidatorsMessage(preferredLanguages: locale.rLanguages),
