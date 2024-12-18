a22c577eb5aec2416fcdda979817db0630ab7c11
Radmir Dzhurabaev <dzhurabaev@soramitsu.co.jp>
2024-12-18 15:08:47 +0500

    [#FLW-5091] The icons on the banners are on the wrong places

--- fearless/Modules/DappBrowser/DappBrowserViewController.swift
+++ fearless/Modules/DappBrowser/DappBrowserViewController.swift
@@ -216,7 +216,7 @@ extension DappBrowserViewController: UITableViewDelegate {
     func tableView(_: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         switch viewModel[indexPath.section] {
         case .featured:
-            return 140
+            return UIScreen.width / 3.4
         case .section:
             return 64
         }
