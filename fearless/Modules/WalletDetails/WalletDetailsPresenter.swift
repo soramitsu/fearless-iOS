963910f5811e99b35ab9f3ba14e69da1d7238125
William Richter <richter@soramitsu.co.jp>
2025-10-14 11:25:41 +0200

    fix(wallet-details): return nil in compactMap closure instead of casting to optional to satisfy Swift 6 inference
    Signed-off-by: William Richter <richter@soramitsu.co.jp>

--- fearless/Modules/WalletDetails/WalletDetailsPresenter.swift
+++ fearless/Modules/WalletDetails/WalletDetailsPresenter.swift
@@ -221,13 +221,13 @@ private extension WalletDetailsPresenter {
                         return .polkascan(url: url)
                     }
                 case .unknown:
-                    return nil as ChainAction?
+                    return nil
                 case .oklink:
                     if $0.types.contains(.account), let url = $0.explorerUrl(for: address, type: .account) {
                         return .oklink(url: url)
                     }
                 }
-                return nil as ChainAction?
+                return nil
             }
             actions.append(contentsOf: explorerActions)
         }
