00dcbfc8c18d952190251c89a90e104940518245
William Richter <richter@soramitsu.co.jp>
2025-10-14 09:02:59 +0200

    fix(staking): use contains(where:) and tokenProperties.stacking in StakingAssetSettings to select chains with staking support
    Signed-off-by: William Richter <richter@soramitsu.co.jp>

--- fearless/Common/Storage/StakingAssetSettings.swift
+++ fearless/Common/Storage/StakingAssetSettings.swift
@@ -50,10 +50,11 @@ final class StakingAssetSettings: PersistentValueSettings<ChainAsset> {
             }
 
             let maybeChain = chains.first { chain in
-                chain.assets.contains { $0.staking != nil } && self?.wallet.fetch(for: chain.accountRequest()) != nil
+                chain.assets.contains(where: { $0.tokenProperties?.stacking != nil }) &&
+                    self?.wallet.fetch(for: chain.accountRequest()) != nil
             }
 
-            let maybeAsset = maybeChain?.assets.first { $0.staking != nil }
+            let maybeAsset = maybeChain?.assets.first { $0.tokenProperties?.stacking != nil }
 
             if let chain = maybeChain, let asset = maybeAsset {
                 self?.settings.stakingAsset = ChainAssetId(chainId: chain.chainId, assetId: asset.id)
