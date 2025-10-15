f885c171ca8f4ad71e26c5b6ac5da332ccc3f1ba
William Richter <richter@soramitsu.co.jp>
2025-10-15 09:23:53 +0200

    fix: add Data.bytes compatibility shim for Swift 6
    Provide  to satisfy existing call sites (e.g., WalletConnect/Ethereum).

--- fearless/Common/Compatibility/Data+Bytes.swift
+++ fearless/Common/Compatibility/Data+Bytes.swift
@@ -1,5 +1,8 @@
 import Foundation
 
-public extension Data {
+// Compatibility shim: some code and dependencies expect `Data.bytes: [UInt8]`.
+// In Swift 6, prefer `Array(data)` but provide this for source compatibility.
+extension Data {
     var bytes: [UInt8] { Array(self) }
 }
+
