f8d2ad74b4d75cc12b5015b7957900e2fa60755d
William Richter <richter@soramitsu.co.jp>
2025-10-16 10:17:39 +0200

    fix(wc): mark Redirect init as throwing with try! in AppMetadata factory
    Signed-off-by: William Richter <richter@soramitsu.co.jp>

--- fearless/ApplicationLayer/Services/WalletConnect/Model/AppMetadata.swift
+++ fearless/ApplicationLayer/Services/WalletConnect/Model/AppMetadata.swift
@@ -8,7 +8,7 @@ extension AppMetadata {
             description: "Defi wallet",
             url: "https://fearlesswallet.io",
             icons: ["https://raw.githubusercontent.com/soramitsu/shared-features-utils/master/icons/FW%20icon%20128.png"],
-            redirect: AppMetadata.Redirect(native: "fearless://", universal: nil)
+            redirect: try! AppMetadata.Redirect(native: "fearless://", universal: nil)
         )
     }
 }
