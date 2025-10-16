781a4d4ba6d16e92302e453bc699160091f5d460
William Richter <richter@soramitsu.co.jp>
2025-10-16 14:31:48 +0200

    chore(wc): revert to non-throwing WalletConnectURI initializer to avoid spurious try? warning
    Signed-off-by: William Richter <richter@soramitsu.co.jp>

--- fearless/ApplicationLayer/Services/WalletConnect/WalletConnectService.swift
+++ fearless/ApplicationLayer/Services/WalletConnect/WalletConnectService.swift
@@ -101,7 +101,7 @@ final class WalletConnectServiceImpl: WalletConnectService {
     }
 
     func connect(uri: String) async throws {
-        guard let walletConnectUri = try? WalletConnectURI(string: uri) else {
+        guard let walletConnectUri = WalletConnectURI(string: uri) else {
             let preferredLanguages = LocalizationManager.shared.selectedLocale.rLanguages
             let title = R.string.localizable.walletConnectInvalidUrlTitle(preferredLanguages: preferredLanguages)
             let message = R.string.localizable.walletConnectInvalidUrlMessage(preferredLanguages: preferredLanguages)
