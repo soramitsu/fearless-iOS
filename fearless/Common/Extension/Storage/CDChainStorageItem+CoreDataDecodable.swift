53b54712d41b23bdfb0ec30ed5a838be861a717b
William Richter <richter@soramitsu.co.jp>
2025-10-17 09:58:32 +0200

    fix: attach CoreDataCodable conformance to storage module type
    Change extension to  so generic
    repository helpers see the conformance when U is module-qualified.
    
    Signed-off-by: William Richter <richter@soramitsu.co.jp>

--- fearless/Common/Extension/Storage/CDChainStorageItem+CoreDataDecodable.swift
+++ fearless/Common/Extension/Storage/CDChainStorageItem+CoreDataDecodable.swift
@@ -1,8 +1,9 @@
 import Foundation
 import CoreData
 import RobinHood
+import SSFAssetManagmentStorage
 
-extension CDChainStorageItem: CoreDataCodable {
+extension SSFAssetManagmentStorage.CDChainStorageItem: CoreDataCodable {
     public func populate(from decoder: Decoder, using _: NSManagedObjectContext) throws {
         let container = try decoder.container(keyedBy: ChainStorageItem.CodingKeys.self)
 
