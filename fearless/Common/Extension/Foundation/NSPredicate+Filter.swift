3f5a9d4f4fdab351c306eb1f42028f7970f44108
William Richter <richter@soramitsu.co.jp>
2025-10-17 16:19:08 +0200

    fix: qualify Core Data key paths in NSPredicate helpers
    - Import SSFAssetManagmentStorage
    - Point all #keyPath references to module-qualified Core Data classes
    
    Signed-off-by: William Richter <richter@soramitsu.co.jp>

--- fearless/Common/Extension/Foundation/NSPredicate+Filter.swift
+++ fearless/Common/Extension/Foundation/NSPredicate+Filter.swift
@@ -1,13 +1,13 @@
 import Foundation
 import IrohaCrypto
 import SSFModels
-import SSFAccountManagmentStorage
+import SSFAssetManagmentStorage
 
 extension NSPredicate {
     // TODO: Remove
     static func filterAccountBy(networkType: SNAddressType) -> NSPredicate {
         let rawValue = Int16(networkType.rawValue)
-        return NSPredicate(format: "%K == %d", #keyPath(CDMetaAccount.order), rawValue)
+        return NSPredicate(format: "%K == %d", #keyPath(SSFAssetManagmentStorage.CDMetaAccount.order), rawValue)
     }
 
     static func filterTransactionsBy(address: String) -> NSPredicate {
@@ -19,27 +19,27 @@ extension NSPredicate {
     }
 
     static func filterTransactionsBySender(address: String) -> NSPredicate {
-        NSPredicate(format: "%K == %@", #keyPath(CDTransactionHistoryItem.sender), address)
+        NSPredicate(format: "%K == %@", #keyPath(SSFAssetManagmentStorage.CDTransactionHistoryItem.sender), address)
     }
 
     static func filterTransactionsByReceiver(address: String) -> NSPredicate {
-        NSPredicate(format: "%K == %@", #keyPath(CDTransactionHistoryItem.receiver), address)
+        NSPredicate(format: "%K == %@", #keyPath(SSFAssetManagmentStorage.CDTransactionHistoryItem.receiver), address)
     }
 
     static func filterContactsByTarget(address: String) -> NSPredicate {
-        NSPredicate(format: "%K == %@", #keyPath(CDContactItem.targetAddress), address)
+        NSPredicate(format: "%K == %@", #keyPath(SSFAssetManagmentStorage.CDContactItem.targetAddress), address)
     }
 
     static func filterRuntimeMetadataItemsBy(identifier: String) -> NSPredicate {
-        NSPredicate(format: "%K == %@", #keyPath(CDRuntimeMetadataItem.identifier), identifier)
+        NSPredicate(format: "%K == %@", #keyPath(SSFAssetManagmentStorage.CDRuntimeMetadataItem.identifier), identifier)
     }
 
     static func filterStorageItemsBy(identifier: String) -> NSPredicate {
-        NSPredicate(format: "%K == %@", #keyPath(CDChainStorageItem.identifier), identifier)
+        NSPredicate(format: "%K == %@", #keyPath(SSFAssetManagmentStorage.CDChainStorageItem.identifier), identifier)
     }
 
     static func filterByIdPrefix(_ prefix: String) -> NSPredicate {
-        NSPredicate(format: "%K BEGINSWITH %@", #keyPath(CDChainStorageItem.identifier), prefix)
+        NSPredicate(format: "%K BEGINSWITH %@", #keyPath(SSFAssetManagmentStorage.CDChainStorageItem.identifier), prefix)
     }
 
     static func filterByStash(_ address: String) -> NSPredicate {
@@ -58,16 +58,16 @@ extension NSPredicate {
 
         let substrateAccountFilter = NSPredicate(
             format: "%K == %@",
-            #keyPath(CDMetaAccount.substrateAccountId), hexAccountId
+            #keyPath(SSFAssetManagmentStorage.CDMetaAccount.substrateAccountId), hexAccountId
         )
 
         let ethereumAccountFilter = NSPredicate(
             format: "%K == %@",
-            #keyPath(CDMetaAccount.ethereumAddress), hexAccountId
+            #keyPath(SSFAssetManagmentStorage.CDMetaAccount.ethereumAddress), hexAccountId
         )
 
         let chainAccountFilter = NSPredicate(
-            format: "ANY %K == %@", #keyPath(CDMetaAccount.chainAccounts.accountId), hexAccountId
+            format: "ANY %K == %@", #keyPath(SSFAssetManagmentStorage.CDMetaAccount.chainAccounts.accountId), hexAccountId
         )
 
         return NSCompoundPredicate(orPredicateWithSubpredicates: [
@@ -78,22 +78,22 @@ extension NSPredicate {
     }
 
     static func selectedMetaAccount() -> NSPredicate {
-        NSPredicate(format: "%K == true", #keyPath(CDMetaAccount.isSelected))
+        NSPredicate(format: "%K == true", #keyPath(SSFAssetManagmentStorage.CDMetaAccount.isSelected))
     }
 
     static func relayChains() -> NSPredicate {
-        NSPredicate(format: "%K = nil", #keyPath(CDChain.parentId))
+        NSPredicate(format: "%K = nil", #keyPath(SSFAssetManagmentStorage.CDChain.parentId))
     }
 
     static func chainBy(identifier: ChainModel.Id) -> NSPredicate {
-        NSPredicate(format: "%K == %@", #keyPath(CDChain.chainId), identifier)
+        NSPredicate(format: "%K == %@", #keyPath(SSFAssetManagmentStorage.CDChain.chainId), identifier)
     }
 
     static func hasCrowloans() -> NSPredicate {
-        NSPredicate(format: "%K == true", #keyPath(CDChain.hasCrowdloans))
+        NSPredicate(format: "%K == true", #keyPath(SSFAssetManagmentStorage.CDChain.hasCrowdloans))
     }
 
     static func enabledCHain() -> NSPredicate {
-        NSPredicate(format: "%K == false", #keyPath(CDChain.disabled))
+        NSPredicate(format: "%K == false", #keyPath(SSFAssetManagmentStorage.CDChain.disabled))
     }
 }
