94bd0fb99389287de3c2298b03b5056685a0695a
William Richter <richter@soramitsu.co.jp>
2025-10-17 08:01:37 +0200

    fix(coredata): add aliases to SSFAssetManagmentStorage Core Data entities to satisfy references without duplicate codegen
    Signed-off-by: William Richter <richter@soramitsu.co.jp>

---
+++ fearless/Common/CoreData/CoreDataAliases.swift
@@ -0,0 +1,27 @@
+import Foundation
+import SSFAssetManagmentStorage
+
+// Alias Core Data entity classes provided by SSFAssetManagmentStorage so existing
+// code can refer to unqualified names without generating duplicates in the app.
+
+public typealias CDChain = SSFAssetManagmentStorage.CDChain
+public typealias CDAsset = SSFAssetManagmentStorage.CDAsset
+public typealias CDChainAsset = SSFAssetManagmentStorage.CDChainAsset
+public typealias CDChainNode = SSFAssetManagmentStorage.CDChainNode
+public typealias CDChainStorageItem = SSFAssetManagmentStorage.CDChainStorageItem
+public typealias CDRuntimeMetadataItem = SSFAssetManagmentStorage.CDRuntimeMetadataItem
+public typealias CDAccountInfo = SSFAssetManagmentStorage.CDAccountInfo
+public typealias CDStashItem = SSFAssetManagmentStorage.CDStashItem
+public typealias CDSingleValue = SSFAssetManagmentStorage.CDSingleValue
+public typealias CDChainSettings = SSFAssetManagmentStorage.CDChainSettings
+public typealias CDMetaAccount = SSFAssetManagmentStorage.CDMetaAccount
+public typealias CDChainAccount = SSFAssetManagmentStorage.CDChainAccount
+public typealias CDPolkaswapRemoteSettings = SSFAssetManagmentStorage.CDPolkaswapRemoteSettings
+public typealias CDPhishingItem = SSFAssetManagmentStorage.CDPhishingItem
+public typealias CDPriceData = SSFAssetManagmentStorage.CDPriceData
+public typealias CDPriceProvider = SSFAssetManagmentStorage.CDPriceProvider
+public typealias CDContactItem = SSFAssetManagmentStorage.CDContactItem
+public typealias CDExternalApi = SSFAssetManagmentStorage.CDExternalApi
+public typealias CDXcmAvailableDestination = SSFAssetManagmentStorage.CDXcmAvailableDestination
+public typealias CDXcmAvailableAsset = SSFAssetManagmentStorage.CDXcmAvailableAsset
+public typealias CDChainXcmConfig = SSFAssetManagmentStorage.CDChainXcmConfig
