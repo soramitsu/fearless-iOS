433e7dc984bb171fd59b860e539f644d13cfde9a
William Richter <richter@soramitsu.co.jp>
2025-10-27 10:22:25 +0100

    fix(tests): disambiguate ChainAccountResponse/MetaAccountModel in tests via aliases
    Signed-off-by: William Richter <richter@soramitsu.co.jp>

---
+++ fearlessTests/Support/TestTypealiases.swift
@@ -0,0 +1,8 @@
+import Foundation
+@testable import fearless
+import SSFModels
+
+// Disambiguate types that exist both in fearless and SSFModels for the test target.
+public typealias ChainAccountResponse = fearless.ChainAccountResponse
+public typealias MetaAccountModel = fearless.MetaAccountModel
+
