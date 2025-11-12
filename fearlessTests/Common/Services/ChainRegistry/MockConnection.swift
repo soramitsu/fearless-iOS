3f850da547535fbfacf74ea25e5b30794073f2f5
William Richter <richter@soramitsu.co.jp>
2025-11-12 13:45:22 +0100

    test: replace MocksBasket import with local JSONRPCEngine test double to fix module availability on CI
    Signed-off-by: William Richter <richter@soramitsu.co.jp>

--- fearlessTests/Common/Services/ChainRegistry/MockConnection.swift
+++ fearlessTests/Common/Services/ChainRegistry/MockConnection.swift
@@ -1,6 +1,52 @@
 import Foundation
 @testable import fearless
-import MocksBasket
+import SSFUtils
 
-// Use SSF MocksBasket JSONRPCEngineMock which conforms to JSONRPCEngine
-final class MockConnection: JSONRPCEngineMock {}
+// Lightweight test double conforming to SSFUtils.JSONRPCEngine used by chain registry tests.
+final class MockConnection: JSONRPCEngine {
+    var url: URL?
+    var pendingEngineRequests: [JSONRPCRequest] = []
+
+    private var nextId: UInt16 = 1
+
+    func callMethod<P: Codable, T: Decodable>(
+        _ method: String,
+        params: P?,
+        options: JSONRPCOptions,
+        completion closure: ((Result<T, Error>) -> Void)?
+    ) throws -> UInt16 {
+        let id = generateRequestId()
+        // Immediately fail calls in tests that don’t expect networking
+        closure?(.failure(JSONRPCEngineError.clientCancelled))
+        return id
+    }
+
+    func subscribe<P: Codable, T: Decodable>(
+        _ method: String,
+        params: P?,
+        updateClosure: @escaping (T) -> Void,
+        failureClosure: @escaping (Error, Bool) -> Void
+    ) throws -> UInt16 {
+        let id = generateRequestId()
+        // Do not deliver updates; just track a pending subscription
+        return id
+    }
+
+    func cancelForIdentifier(_ identifier: UInt16) {}
+
+    func generateRequestId() -> UInt16 {
+        defer { nextId &+= 1 }
+        return nextId
+    }
+
+    func addSubscription(_ subscription: JSONRPCSubscribing) {
+        pendingEngineRequests.append(
+            JSONRPCRequest(requestId: subscription.requestId, data: subscription.requestData, options: subscription.requestOptions, responseHandler: nil)
+        )
+    }
+
+    func reconnect(url: URL) { self.url = url }
+    func connectIfNeeded() {}
+    func disconnectIfNeeded() {}
+    func unsubsribe(_ identifier: UInt16) throws {}
+}
