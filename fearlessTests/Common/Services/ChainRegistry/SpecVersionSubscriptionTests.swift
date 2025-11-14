274178597ba1bcb84e316342823303fd015aa2d2
William Richter <richter@soramitsu.co.jp>
2025-11-14 11:26:46 +0100

    test: use local runtime version engine stub
    Signed-off-by: William Richter <richter@soramitsu.co.jp>

--- fearlessTests/Common/Services/ChainRegistry/SpecVersionSubscriptionTests.swift
+++ fearlessTests/Common/Services/ChainRegistry/SpecVersionSubscriptionTests.swift
@@ -9,7 +9,7 @@ class SpecVersionSubscriptionTests: XCTestCase {
 
         let chain = ChainModelGenerator.generate(count: 1).first!
         let runtimeSyncService = MockRuntimeSyncServiceProtocol()
-        let connection = MockJSONRPCEngine()
+        let connection = RuntimeVersionJSONRPCEngineStub()
 
         let subscription = SpecVersionSubscription(
             chainId: chain.chainId,
@@ -20,30 +20,6 @@ class SpecVersionSubscriptionTests: XCTestCase {
         let version = RuntimeVersion(specVersion: 1, transactionVersion: 2)
 
         // when
-
-        stub(connection) { stub in
-            stub.subscribe(
-                any(),
-                params: any([String].self),
-                updateClosure: any(),
-                failureClosure: any()
-            ).then { (_, _, updateClosure: @escaping (RuntimeVersionUpdate) -> Void, _) in
-                DispatchQueue.global().async {
-                    let update = RuntimeVersionUpdate(
-                        jsonrpc: "2.0",
-                        method: RPCMethod.runtimeVersionSubscribe,
-                        params: JSONRPCSubscriptionUpdate.Result(
-                            result: version,
-                            subscription: ""
-                        )
-                    )
-
-                    updateClosure(update)
-                }
-
-                return 0
-            }
-        }
 
         let expectation = XCTestExpectation()
 
@@ -56,8 +32,68 @@ class SpecVersionSubscriptionTests: XCTestCase {
 
         subscription.subscribe()
 
+        DispatchQueue.global().async {
+            let update = RuntimeVersionUpdate(
+                jsonrpc: "2.0",
+                method: RPCMethod.runtimeVersionSubscribe,
+                params: JSONRPCSubscriptionUpdate.Result(
+                    result: version,
+                    subscription: ""
+                )
+            )
+
+            connection.emit(update: update)
+        }
+
         // then
 
         wait(for: [expectation], timeout: 10)
+    }
+}
+
+private final class RuntimeVersionJSONRPCEngineStub: JSONRPCEngine {
+    var url: URL?
+    var pendingEngineRequests: [JSONRPCRequest] { [] }
+
+    private var updateHandler: ((RuntimeVersionUpdate) -> Void)?
+
+    func callMethod<P: Encodable, T: Decodable>(
+        _ method: String,
+        params: P?,
+        options: JSONRPCOptions,
+        completion closure: ((Result<T, Error>) -> Void)?
+    ) throws -> UInt16 {
+        throw JSONRPCEngineError.clientCancelled
+    }
+
+    func subscribe<P: Encodable, T: Decodable>(
+        _ method: String,
+        params: P?,
+        updateClosure: @escaping (T) -> Void,
+        failureClosure: @escaping (Error, Bool) -> Void
+    ) throws -> UInt16 {
+        precondition(T.self == RuntimeVersionUpdate.self, "Unsupported subscription type \(T.self)")
+
+        updateHandler = { update in
+            updateClosure(update as! T) // swiftlint:disable:this force_cast
+        }
+
+        return 0
+    }
+
+    func cancelForIdentifier(_ identifier: UInt16) {}
+
+    func generateRequestId() -> UInt16 { 0 }
+
+    func addSubscription(_ subscription: JSONRPCSubscribing) {}
+
+    func reconnect(url: URL) { self.url = url }
+
+    func connectIfNeeded() {}
+
+    func disconnectIfNeeded() {}
+
+    func emit(update: RuntimeVersionUpdate) {
+        updateHandler?(update)
     }
 }
