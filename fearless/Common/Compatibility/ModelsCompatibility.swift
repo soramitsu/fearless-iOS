409d59c81592aa418f8f0e3e7dd8945f0a4d1c72
William Richter <richter@soramitsu.co.jp>
2026-04-20 16:02:00 +0200

    refactor: remove unused models compatibility shim
    Signed-off-by: William Richter <richter@soramitsu.co.jp>

--- fearless/Common/Compatibility/ModelsCompatibility.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import SSFModels
-
-public extension ChainModel.BlockExplorer {
-    init?(type: String, url: URL, apiKey _: String?) {
-        self.init(type: type, url: url)
-    }
-}
