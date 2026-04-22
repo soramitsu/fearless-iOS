b7896f20ce57c846d65008d96174c32cb6ee4ff2
William Richter <richter@soramitsu.co.jp>
2026-04-22 13:11:35 +0200

    test: wire TON compatibility tests into target
    Signed-off-by: William Richter <richter@soramitsu.co.jp>

--- fearlessTests/ApplicationLayer/Services/FeatureToggle/TonChainSelectionTests.swift
+++ fearlessTests/ApplicationLayer/Services/FeatureToggle/TonChainSelectionTests.swift
@@ -55,3 +55,35 @@ final class TonChainSelectionTests: XCTestCase {
         )
     }
 }
+
+final class TonCompatibilityTests: XCTestCase {
+    func testTonAssetTypeMapsNormal() {
+        let type: SubstrateAssetType? = .normal
+
+        XCTAssertEqual(type.tonAssetType, .normal)
+    }
+
+    func testTonAssetTypeMapsJettonForNonNormalType() {
+        let type: SubstrateAssetType? = .ormlAsset
+
+        XCTAssertEqual(type.tonAssetType, .jetton)
+    }
+
+    func testTonAssetTypeMapsNoneForMissingType() {
+        let type: SubstrateAssetType? = nil
+
+        XCTAssertEqual(type.tonAssetType, .none)
+    }
+
+    func testDataTailReturnsSuffixWhenDataIsLongerThanLength() {
+        let data = Data([0x01, 0x02, 0x03, 0x04])
+
+        XCTAssertEqual(data.tail(2), Data([0x03, 0x04]))
+    }
+
+    func testDataTailReturnsOriginalDataWhenLengthExceedsCount() {
+        let data = Data([0xAA, 0xBB])
+
+        XCTAssertEqual(data.tail(8), data)
+    }
+}
