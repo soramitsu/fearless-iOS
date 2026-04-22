af1d6aafe432aa6c4cef72ea61c73adc1a1763e0
William Richter <richter@soramitsu.co.jp>
2026-04-22 19:42:51 +0200

    test: add account factory test compatibility aliases
    Signed-off-by: William Richter <richter@soramitsu.co.jp>

--- fearlessTests/Helper/TestTypeAliases.swift
+++ fearlessTests/Helper/TestTypeAliases.swift
@@ -3,13 +3,30 @@ import Foundation
 import SSFModels
 import SSFRuntimeCodingService
 import FearlessUtils
+import RobinHood
 
 // Disambiguate model types between fearless and SSFModels in tests
 typealias MetaAccountModel = fearless.MetaAccountModel
 typealias ChainAccountResponse = fearless.ChainAccountResponse
 typealias ChainAccountInfo = fearless.ChainAccountInfo
 typealias ChainModel = SSFModels.ChainModel
+typealias ChainNodeModel = SSFModels.ChainNodeModel
 typealias ChainAsset = SSFModels.ChainAsset
 typealias AssetModel = SSFModels.AssetModel
 typealias CryptoType = FearlessUtils.CryptoType
 typealias RuntimeProviderProtocol = SSFRuntimeCodingService.RuntimeProviderProtocol
+
+// Keep generated legacy mocks compiling while production protocol shims are removed.
+extension AccountRepositoryFactoryProtocol {
+    @available(*, deprecated, message: "Use createMetaAccountRepository(for:sortDescriptors:) instead")
+    func createRepository() -> AnyDataProviderRepository<fearless.MetaAccountModel> {
+        createMetaAccountRepository(for: nil, sortDescriptors: [])
+    }
+
+    @available(*, deprecated, message: "Use createMetaAccountRepository(for:sortDescriptors:) instead")
+    func createAccountRepository(
+        for _: fearless.SNAddressType
+    ) -> AnyDataProviderRepository<fearless.MetaAccountModel> {
+        createMetaAccountRepository(for: nil, sortDescriptors: [])
+    }
+}
